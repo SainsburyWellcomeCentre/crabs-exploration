6162b1b661434663bf36a7edc695cf547122ed24
sfmig <33267254+sfmig@users.noreply.github.com>
2023-10-30 10:28:38 +0000

    add frame extraction case insensitive test

--- tests/test_frame_extraction.py
+++ tests/test_frame_extraction.py
@@ -1,8 +1,58 @@
 from pathlib import Path
+
+import pytest
 import sys
 import subprocess
 
 
+from bboxes_labelling.extract_frames_to_label_w_sleap import get_list_of_sleap_videos
+
+
+@pytest.fixture(autouse=True, scope="class")
+def input_video_dir():
+    return Path(__file__).parent / "data" / "clips"
+
+
+class TestsFrameExtraction:
+    def test_extension_case_insensitive(self, input_video_dir):
+        """
+        Tests that the function that computes the list of SLEAP videos
+        is case-insensitive for the user-input extension.
+
+        Parameters
+        ----------
+        input_video_dir : pathlib.Path
+            Path to the input video directory
+        """
+        # build list of video locations
+        list_video_locations = [input_video_dir]
+
+        # get unique extensions for all files in the
+        # input directory
+        # TODO: check they are all video files?
+        list_files = [
+            f
+            for f in list_video_locations[0].glob("*")
+            if f.is_file() and not f.name.startswith(".")
+        ]
+        list_unique_extensions = list({f.suffix[1:] for f in list_files})
+
+        # force the user-input extensions to be of the opposite case
+        list_user_extensions = [ext.lower() for ext in list_unique_extensions]
+        list_user_extensions = list(set(list_user_extensions))
+
+        # compute list of SLEAP videos for the given user extensions
+        list_sleap_videos = get_list_of_sleap_videos(
+            list_video_locations,
+            list_user_extensions,
+        )
+
+        # check list of SLEAP videos matches the list of files
+        assert len(list_sleap_videos) == len(list_files)
+
+
+# --------------------------
+# TODO: make fixtures
 script_path = (
     Path(__file__).parent
     / ".."
@@ -30,8 +80,10 @@ sample_input_params = {
     "n_clusters": "5",
     "per_cluster": "1",
 }
+# ----------
 
 
+# TODO: add to class if required
 def test_help():
     result = subprocess.run(
         [python_interpreter, script_path, "-h"],
@@ -42,6 +94,7 @@ def test_help():
     assert result.returncode == 0
 
 
+# TODO: add to class if required
 def test_small_frame_extraction_one_video():
     # TODO: can these be fixtures?
     list_kys_modif = ["--" + k for k in sample_input_params.keys()]
