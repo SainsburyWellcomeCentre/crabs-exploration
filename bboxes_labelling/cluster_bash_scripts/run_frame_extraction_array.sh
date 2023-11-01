4a38befa7a0c6c656ff964b67d5bf10419b3fd69
sfmig <33267254+sfmig@users.noreply.github.com>
2023-11-01 17:15:08 +0000

    edit bash script for Aug23 day3

--- bboxes_labelling/cluster_bash_scripts/run_frame_extraction_array.sh
+++ bboxes_labelling/cluster_bash_scripts/run_frame_extraction_array.sh
@@ -10,11 +10,11 @@
 #SBATCH -e slurm_array.%N.%A-%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s.minano@ucl.ac.uk
-#SBATCH --array=0-1%4
+#SBATCH --array=0-3%4
 
 #-------
 # NOTE!!
-# with "SBATCH --array=0-1%4" ---> runs n separate jobs, but not more than m at a time.
+# with "SBATCH --array=0-n%m" ---> runs n separate jobs, but not more than m at a time.
 # the number of array jobs should match the number of input files
 
 # ---------------------
@@ -29,9 +29,10 @@ module load SLEAP
 # TODO: have list here?
 # INPUT_DATA_LIST=($(<input.list))
 INPUT_DATA_LIST=(
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T003.MOV"
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T004.MOV"
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T005.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/10.08.2023-Day3/10.08.2023-01-Left.mov"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/10.08.2023-Day3/10.08.2023-01-Right.mov"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/10.08.2023-Day3/10.08.2023-02-Left.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/10.08.2023-Day3/10.08.2023-02-Right.MOV"
 )
 
 # Check len(list of input data) matches max SLURM_ARRAY_TASK_COUNT
@@ -45,24 +46,24 @@ fi
 # output data location
 # ----------------------
 OUTPUT_DIR=/ceph/zoo/users/sminano/crabs_bboxes_labels
-OUTPUT_SUBDIR="20230808_zoomed_in_combined"
+OUTPUT_SUBDIR="Aug2023_day3"
 
 # ----------------------
 # parameters
 # ----------------------
 PARAM_VIDEO_EXT=MOV
-PARAM_INI_SAMPLES=300
-PARAM_SCALE=0.75
+PARAM_INI_SAMPLES=500
+PARAM_SCALE=0.5
 PARAM_N_COMPONENTS=5
 PARAM_N_CLUSTERS=5
-PARAM_PER_CLUSTER=8
+PARAM_PER_CLUSTER=10
 
 # ----------------------
 # script location
 # ----------------------
 # assumes repo located at '/ceph/scratch/sminano'
 SCRATCH_PERSONAL_DIR=/ceph/scratch/sminano
-SCRIPT_DIR=$SCRATCH_PERSONAL_DIR/crabs-exploration/"bboxes labelling"
+SCRIPT_DIR=$SCRATCH_PERSONAL_DIR/crabs-exploration/bboxes_labelling
 
 
 # -------------------
