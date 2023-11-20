224b2bda785d0fe5045ad74f668493180ce77849
sfmig <33267254+sfmig@users.noreply.github.com>
2023-11-20 16:12:23 +0100

    Bash script to reencode videos (#31)
    * edit bash script to extract frames
    
    * add bash script for reencoding
    
    * rename notebooks dir
    
    * remove commented files
    
    * minor changes to frame extraction script for slurm array job
    
    * print ffmpeg version  to slurm logs
    
    * remove apple encoder settings
    
    * fix path
    
    * add clarification for array job syntax

--- bash_scripts/run_frame_extraction_array.sh
+++ bash_scripts/run_frame_extraction_array.sh
@@ -10,12 +10,13 @@
 #SBATCH -e slurm_array.%N.%A-%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s.minano@ucl.ac.uk
-#SBATCH --array=0-1%4
+#SBATCH --array=0-6%4
 
 #-------
-# NOTE!!
-# with "SBATCH --array=0-1%4" ---> runs n separate jobs, but not more than m at a time.
+# NOTE
+# with "SBATCH --array=0-1%4" ---> runs n+1 separate jobs, but not more than m at a time.
 # the number of array jobs should match the number of input files
+#-------
 
 # ---------------------
 # Load required modules
@@ -29,9 +30,13 @@ module load SLEAP
 # TODO: have list here?
 # INPUT_DATA_LIST=($(<input.list))
 INPUT_DATA_LIST=(
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T003.MOV"
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T004.MOV"
-    "/ceph/zoo/raw/CrabField/ramalhete_2021/camera2/NINJAV_S001_S001_T005.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-01-Left.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-01-Right.mov"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-02-Left.mov"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-02-Right.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-03-Left.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-04-Left.MOV"
+    "/ceph/zoo/raw/CrabField/ramalhete_2023/09.08.2023-Day2/09.08.2023-04-Right.MOV"
 )
 
 # Check len(list of input data) matches max SLURM_ARRAY_TASK_COUNT
@@ -45,24 +50,24 @@ fi
 # output data location
 # ----------------------
 OUTPUT_DIR=/ceph/zoo/users/sminano/crabs_bboxes_labels
-OUTPUT_SUBDIR="20230808_zoomed_in_combined"
+OUTPUT_SUBDIR="20230816_ramalhete2023_day2_combined"
 
 # ----------------------
 # parameters
 # ----------------------
 PARAM_VIDEO_EXT=MOV
-PARAM_INI_SAMPLES=300
-PARAM_SCALE=0.75
+PARAM_INI_SAMPLES=500
+PARAM_SCALE=0.5
 PARAM_N_COMPONENTS=5
 PARAM_N_CLUSTERS=5
-PARAM_PER_CLUSTER=8
+PARAM_PER_CLUSTER=10
 
 # ----------------------
 # script location
 # ----------------------
 # assumes repo located at '/ceph/scratch/sminano'
 SCRATCH_PERSONAL_DIR=/ceph/scratch/sminano
-SCRIPT_DIR=$SCRATCH_PERSONAL_DIR/crabs-exploration/"bboxes labelling"
+SCRIPT_DIR=$SCRATCH_PERSONAL_DIR/crabs-exploration/crabs/bboxes_labelling
 
 
 # -------------------
