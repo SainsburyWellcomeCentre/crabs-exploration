4b44c0dd7b54824b62f9e8f547064811573b3aac
sfmig <33267254+sfmig@users.noreply.github.com>
2023-11-23 13:44:11 +0000

    Update crabs/stereo_calibration/__init__.py
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>
    Signed-off-by: sfmig <33267254+sfmig@users.noreply.github.com>

--- crabs/stereo_calibration/__init__.py
+++ crabs/stereo_calibration/__init__.py
@@ -1,1 +1,5 @@
-raise NotImplementedError
+msg = """
+The stereo calibration pipeline code is not ready for use yet so we've temporarily blocked it's use!
+See https://github.com/SainsburyWellcomeCentre/crabs-exploration/issues/90 for more details. Create a branch and remove the NotImplementedError to develop!
+"""
+raise NotImplementedError(msg)
