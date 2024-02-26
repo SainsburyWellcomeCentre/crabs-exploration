ca8008ef1fd4054bb224a065959dcedbaad22d26
sfmig <33267254+sfmig@users.noreply.github.com>
2024-02-26 18:19:45 +0000

    refactor case insensitive test

--- tests/test_integration/test_frame_extraction.py
+++ tests/test_integration/test_frame_extraction.py
@@ -12,40 +12,51 @@ def input_video_dir():
     return Path(__file__).parents[1] / "data" / "clips"
 
 
-class TestsFrameExtraction:
-    def test_extension_case_insensitive(self, input_video_dir):
-        """
-        Tests that the function that computes the list of SLEAP videos
-        is case-insensitive for the user-input extension.
+@pytest.fixture()
+def list_user_extensions_flipped(input_video_dir):
+    # build list of video files
+    list_files = [
+        f
+        for f in input_video_dir.glob("*")
+        if f.is_file() and not f.name.startswith(".")
+    ]
 
-        Parameters
-        ----------
-        input_video_dir : pathlib.Path
-            Path to the input video directory
-        """
-        # build list of video locations
-        list_video_locations = [input_video_dir]
+    # get unique extensions for all files in the
+    # input directory
+    list_unique_extensions = list({f.suffix[1:] for f in list_files})
 
-        # get unique extensions for all files in the
-        # input directory
-        # TODO: check they are all video files?
-        list_files = [
-            f
-            for f in list_video_locations[0].glob("*")
-            if f.is_file() and not f.name.startswith(".")
-        ]
-        list_unique_extensions = list({f.suffix[1:] for f in list_files})
+    # force the user-input extensions to be of the opposite case
+    list_user_extensions_flip = [ext.lower() for ext in list_unique_extensions]
+    list_user_extensions_flip = list(set(list_user_extensions_flip))
 
-        # force the user-input extensions to be of the opposite case
-        list_user_extensions = [ext.lower() for ext in list_unique_extensions]
-        list_user_extensions = list(set(list_user_extensions))
+    return list_user_extensions_flip
 
-        # compute list of SLEAP videos for the given user extensions
-        list_sleap_videos = get_list_of_sleap_videos(
-            list_video_locations,
-            list_user_extensions,
-        )
 
-        # check list of SLEAP videos matches the list of files
-        assert len(list_sleap_videos) == len(list_files)
-        assert len(list_sleap_videos) == len(list_files)
+def test_extension_case_insensitive(
+    input_video_dir, list_user_extensions_flipped
+):
+    """
+    Tests that the function that computes the list of SLEAP videos
+    is case-insensitive for the user-input extension.
+
+    Parameters
+    ----------
+    input_video_dir : pathlib.Path
+        Path to the input video directory
+    """
+    # build list of video files
+    list_files = [
+        f
+        for f in input_video_dir.glob("*")
+        if f.is_file() and not f.name.startswith(".")
+    ]
+
+    # compute list of SLEAP videos for the given user extensions
+    list_sleap_videos = get_list_of_sleap_videos(
+        [input_video_dir],
+        list_user_extensions_flipped,
+    )
+
+    # check list of SLEAP videos matches the list of files
+    assert len(list_sleap_videos) == len(list_files)
+    assert len(list_sleap_videos) == len(list_files)
