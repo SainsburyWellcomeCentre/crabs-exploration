ea8a20197663a47dfe0c25d131638da6b5515603
sfmig <33267254+sfmig@users.noreply.github.com>
2024-02-26 19:13:16 +0000

    tests passing for extraction from dir with defaults (WIP)

--- tests/test_integration/test_frame_extraction.py
+++ tests/test_integration/test_frame_extraction.py
@@ -7,10 +7,9 @@ from crabs.bboxes_labelling.extract_frames_to_label_w_sleap import (
     get_list_of_sleap_videos,
 )
 
-
-@pytest.fixture(autouse=True, scope="class")
-def input_video_dir():
-    return Path(__file__).parents[1] / "data" / "clips"
+# @pytest.fixture(autouse=True, scope="class")
+# def input_video_dir():
+#     return Path(__file__).parents[1] / "data" / "clips"
 
 
 @pytest.fixture()
@@ -19,36 +18,56 @@ def extract_frames_command():
 
 
 @pytest.fixture()
-def sample_input_video():
-    # TODO: add a very small sample clip!
-    return Path(__file__).parent / "data" / "NINJAV_S001_S001_T003_subclip.mp4"
+def sample_data(tmp_path):
+    input_dir = Path(__file__).parents[1] / "data" / "clips"
+    output_dir = tmp_path
 
-
-@pytest.fixture()
-def sample_input_dir():
-    return Path(__file__).parent / "data"
-
-
-@pytest.fixture()
-def sample_output_dir():
-    return Path(__file__).parent / "output"
-
-
-@pytest.fixture()
-def sample_input_params(sample_output_dir):
     return {
-        "output_path": sample_output_dir,
-        "video_extensions": "mp4",
-        "initial_samples": "5",
-        "scale": "0.5",
-        "n_components": "3",
-        "n_clusters": "5",
-        "per_cluster": "1",
+        "input_dir": input_dir,
+        "output_dir": output_dir,
+        "input_video": input_dir / "NINJAV_S001_S001_T003_subclip.mp4",
+        "extract_frames_params": {
+            "output_path": str(output_dir),
+            "video_extensions": "mp4",
+            "initial_samples": "5",
+            "scale": "0.5",
+            "n_components": "3",
+            "n_clusters": "5",
+            "per_cluster": "1",
+            "compute_features_per_video": "",
+        },
     }
 
 
+def dict_to_list_of_cli_args(input_params: dict) -> list:
+    """If value is empty string, key is taken as a CLI boolean argument
+
+    Parameters
+    ----------
+    input_params : dict
+        _description_
+
+    Returns
+    -------
+    list
+        _description_
+    """
+
+    list_kys_modified = ["--" + k for k in input_params.keys()]
+    list_cli_args = [
+        elem
+        for pair in zip(list_kys_modified, input_params.values())
+        for elem in pair
+        if elem != ""
+    ]
+
+    return list_cli_args
+
+
 @pytest.fixture()
-def list_user_extensions_flipped(input_video_dir):
+def list_user_extensions_flipped(sample_data):
+    input_video_dir = sample_data["input_dir"]
+
     # build list of video files
     list_files = [
         f
@@ -67,25 +86,16 @@ def list_user_extensions_flipped(input_video_dir):
     return list_user_extensions_flip
 
 
-def test_small_frame_extraction_one_video(
-    extract_frames_command, sample_input_video, sample_input_params
-):
+def test_small_frame_extraction_one_video(extract_frames_command, sample_data):
     # format input parameters as command line arguments
-    list_kys_modif = ["--" + k for k in sample_input_params.keys()]
-    list_non_bool_cli_args = [
-        val
-        for pair in zip(list_kys_modif, list(sample_input_params.values()))
-        for val in pair
-    ]
-    list_bool_cli_args = ["--compute_features_per_video"]
-    list_cli_args = list_non_bool_cli_args + list_bool_cli_args
+    dict_to_list_of_cli_args(sample_data["extract_frames_params"])
 
     result = subprocess.run(
         [
             extract_frames_command,
-            sample_input_video,
-        ]
-        + list_cli_args,
+            str(sample_data["input_video"]),
+        ],
+        # + list_cli_args,
         capture_output=True,
         text=True,
     )
@@ -102,25 +112,16 @@ def test_small_frame_extraction_one_video(
     # check name of files
 
 
-def test_small_frame_extraction_one_dir(
-    extract_frames_command, sample_input_params, sample_input_dir
-):
-    # TODO: can these be fixtures?
-    list_kys_modif = ["--" + k for k in sample_input_params.keys()]
-    list_non_bool_cli_args = [
-        val
-        for pair in zip(list_kys_modif, list(sample_input_params.values()))
-        for val in pair
-    ]
-    list_bool_cli_args = ["--compute_features_per_video"]
-    list_cli_args = list_non_bool_cli_args + list_bool_cli_args
+def test_small_frame_extraction_one_dir(extract_frames_command, sample_data):
+    # format input parameters as command line arguments
+    dict_to_list_of_cli_args(sample_data["extract_frames_params"])
 
     result = subprocess.run(
         [
             extract_frames_command,
-            sample_input_dir,
-        ]
-        + list_cli_args,
+            str(sample_data["input_dir"]),
+        ],
+        # + list_cli_args,  # comment for defaults?
         capture_output=True,
         text=True,
     )
@@ -131,9 +132,7 @@ def test_small_frame_extraction_one_dir(
     # check name of files
 
 
-def test_extension_case_insensitive(
-    input_video_dir, list_user_extensions_flipped
-):
+def test_extension_case_insensitive(sample_data, list_user_extensions_flipped):
     """
     Tests that the function that computes the list of SLEAP videos
     is case-insensitive for the user-input extension.
@@ -143,6 +142,9 @@ def test_extension_case_insensitive(
     input_video_dir : pathlib.Path
         Path to the input video directory
     """
+
+    input_video_dir = sample_data["input_dir"]
+
     # build list of video files
     list_files = [
         f
