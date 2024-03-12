3748049b4e6abfbb65cc7e9acb023b87b0d4c559
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-12 13:58:51 +0000

    use new dataset in datamodule

--- crabs/detection_tracking/datamodules.py
+++ crabs/detection_tracking/datamodules.py
@@ -1,19 +1,25 @@
 import lightning as L
 import torch
 from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
 from torchvision.transforms import transforms
+
+# from torchvision.transforms import v2
+from crabs.detection_tracking.datasets import CrabsCocoDetection
 
 
 class CrabsDataModule(L.LightningDataModule):
-    def __init__(self, data_dir: str, annotations, config):
+    def __init__(
+        self, imgs_path: str, annotations_path, config, split_seed=None
+    ):
         super().__init__()
-        self.data_dir = data_dir
-        self.annotations = annotations
+        self.imgs_path = imgs_path
+        self.annotations_path = annotations_path
         self.config = config
+        self.split_seed = split_seed
 
     def _get_train_transform(self):
         # see https://pytorch.org/vision/stable/transforms.html#v1-or-v2-which-one-should-i-use
+        # https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#transforms
         train_transforms = []
         train_transforms.append(
             transforms.ColorJitter(
@@ -33,7 +39,7 @@ class CrabsDataModule(L.LightningDataModule):
                 sigma=self.config["gaussian_blur_params"]["sigma"],
             )
         )
-        train_transforms.append(transforms.ToTensor())
+        train_transforms.append(transforms.ToTensor())  # ToImage()?
         return train_transforms
 
     def _get_test_val_transform(self):
@@ -41,13 +47,16 @@ class CrabsDataModule(L.LightningDataModule):
         test_transforms.append(transforms.ToTensor())
         return test_transforms
 
-    def _collate_fn(batch):
-        batch = [sample for sample in batch if sample is not None]
+    # def _collate_fn(batch):
+    #     # https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#data-loading-and-training-loop
+    #     # We need a custom fn because the number of bounding boxes varies between images of the same batch
+    #     # why would a sample be None?
+    #     batch = [sample for sample in batch if sample is not None]
 
-        if len(batch) == 0:
-            return tuple()
+    #     if len(batch) == 0:
+    #         return tuple()
 
-        return tuple(zip(*batch))
+    #     return tuple(zip(*batch))
 
     def _compute_splits(self):
         # Compute train/test/val splits
@@ -57,20 +66,17 @@ class CrabsDataModule(L.LightningDataModule):
 
         # Optionally fix the generator for reproducible results
         generator = None
-        if self.config["dataset_split_seed"]:
-            generator = torch.Generator().manual_seed(
-                self.config["dataset_split_seed"]
-            )
+        if self.split_seed:
+            generator = torch.Generator().manual_seed(self.split_seed)
 
         # Instantiate dataset
         # exclude files here? or when creating the dataset?
-        full_dataset = CocoDetection(  # CustomFasterRCNNDataset(  # call COCO
-            self.data_dir,
-            self.annotations,
+        full_dataset = CrabsCocoDetection(
+            self.imgs_path,
+            self.annotations_path,
             transforms=self.train_transform,
             # exclude_files_w_regex------------------
         )
-        full_dataset = wrap_dataset_for_transforms_v2(full_dataset)
 
         # Split data into train/test-val
         # can we specify what to have in train/test?
@@ -133,10 +139,12 @@ class CrabsDataModule(L.LightningDataModule):
         # https://github.com/pytorch/vision/blob/423a1b0ebdea077cc69478812890845741048d2e/references/detection/train.py#L209
         return DataLoader(
             self.train_dataset,
-            batch_size=self.config["train_batch_size"],
+            batch_size=self.config["batch_size_train"],
             shuffle=True,  # A sequential or shuffled sampler will be automatically constructed based on the shuffle argument
             num_workers=self.config["num_workers"],  # set to auto?
-            collate_fn=self._collate_fn,  # --- why do we need it?
+            collate_fn=lambda batch: tuple(
+                zip(*batch)
+            ),  # self._collate_fn,  # --- why do we need it? Yes
             persistent_workers=True,
             # --- why do we need it? to use the same workers across epochs (after loader is exhausted)
             # interesting if it takes a lot of time to spawn workers at the start of the epoch
@@ -144,21 +152,19 @@ class CrabsDataModule(L.LightningDataModule):
         )
 
     def val_dataloader(self, config):
-        # return DataLoader(self.val_set, batch_size=config["val_batch_size"])
         return DataLoader(
             self.val_dataset,
             batch_size=self.config["batch_size_val"],
             shuffle=False,
             num_workers=self.config["num_workers"],
-            collate_fn=self._collate_fn,
+            collate_fn=lambda batch: tuple(zip(*batch)),
         )
 
     def test_dataloader(self, config):
-        # return DataLoader(self.test_set, batch_size=config["test_batch_size"])
         return DataLoader(
             self.test_dataset,
             batch_size=self.config["batch_size_test"],
             shuffle=False,
             num_workers=self.config["num_workers"],
-            collate_fn=self._collate_fn,
+            collate_fn=lambda batch: tuple(zip(*batch)),
         )
