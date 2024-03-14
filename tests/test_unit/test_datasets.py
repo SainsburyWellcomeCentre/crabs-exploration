2eb4e7631dc4565cdd7e48c75e4c4d1e25b736b2
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-14 19:24:45 +0000

    add test for exclude files

--- tests/test_unit/test_datasets.py
+++ tests/test_unit/test_datasets.py
@@ -1,3 +1,5 @@
+import glob
+import mmap
 import shutil
 from pathlib import Path
 
@@ -23,7 +25,7 @@ def annotation_file(tmp_path: Path):
     return str(out_annotation_file)
 
 
-@pytest.mark.parametrize("n_files_to_exclude", [1, 2, 20])
+@pytest.mark.parametrize("n_files_to_exclude", [1, 20])
 def test_exclude_files(annotation_file, n_files_to_exclude):
     # create dataset with all images
     dataset = CrabsCocoDetection(
@@ -31,18 +33,31 @@ def test_exclude_files(annotation_file, n_files_to_exclude):
         [annotation_file],
     )
 
-    # select an image in the dataset
+    # select a subset of images in the dataset
     coco = COCO(annotation_file)
     list_exclude_files = [
         coco.loadImgs(idx)[0]["file_name"]
         for idx in range(1, n_files_to_exclude + 1)
     ]
 
+    # exclude images from the same input data and check number of samples
     dataset_w_exclude = CrabsCocoDetection(
         [SAMPLE_DATASET_DIR],
         [annotation_file],
         list_exclude_files=list_exclude_files,
     )
-
     assert len(dataset) - n_files_to_exclude == len(dataset_w_exclude)
-    # there should also be an additional json file with the expected name
+
+    # get list of output annotation files and check length
+    glob_pattern = str(
+        Path(annotation_file).parent / (Path(annotation_file).stem + "*")
+    )
+    list_output_files = glob.glob(glob_pattern)
+    assert len(list_output_files) == 2
+
+    # check new annotation file contains no references to excluded files
+    new_output_file = [f for f in list_output_files if "_filt_" in f][0]
+    with open(new_output_file) as file:
+        s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
+        for img_to_excl in list_exclude_files:
+            assert s.find(bytes(img_to_excl, "utf-8")) == -1
