873229f8a76506a535e88f4448805ccc2eeb8839
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-15 11:43:47 +0000

    changes to train script for datamodule structure

--- crabs/detection_tracking/train_model.py
+++ crabs/detection_tracking/train_model.py
@@ -1,13 +1,17 @@
 import argparse
 import datetime
+import sys
+from pathlib import Path
 
 import lightning as pl
 import torch
 import yaml  # type: ignore
 
-from crabs.detection_tracking.datamodule import CustomDataModule
+from crabs.detection_tracking.datamodules import CrabsDataModule
 from crabs.detection_tracking.detection_utils import save_model
 from crabs.detection_tracking.models import FasterRCNN
+
+DEFAULT_ANNOTATIONS_FILENAME = "VIA_JSON_combined_coco_gen.json"
 
 
 class DectectorTrain:
@@ -32,37 +36,81 @@ class DectectorTrain:
 
     def __init__(self, args):
         self.config_file = args.config_file
-        self.main_dirs = args.main_dir
-        self.annotation_files = args.annotation_file
+        self.images_dirs = self.prep_img_directories(
+            args.dataset_dirs  # args only?
+        )  # list of paths
+        self.annotation_files = self.prep_annotation_files(
+            args.annotation_files, args.dataset_dirs
+        )  # list of paths
         self.accelerator = args.accelerator
         self.seed_n = args.seed_n
+        self.experiment_name = args.experiment_name
         self.load_config_yaml()
 
     def load_config_yaml(self):
         with open(self.config_file, "r") as f:
             self.config = yaml.safe_load(f)
 
-    def train_model(self):
-        annotations = []
-        for main_dir, annotation_file in zip(
-            self.main_dirs, self.annotation_files
-        ):
-            annotations.append(f"{main_dir}/annotations/{annotation_file}")
+    def prep_img_directories(self, dataset_dirs: list[str]):
+        images_dirs = []
+        for dataset in dataset_dirs:
+            images_dirs.append(str(Path(dataset) / "frames"))
+        return images_dirs
 
-        data_module = CustomDataModule(
-            self.main_dirs, annotations, self.config, self.seed_n
+    def prep_annotation_files(
+        self, input_annotation_files: list[str], dataset_dirs: list[str]
+    ):
+        # prepare list of annotation files
+        annotation_files = []
+
+        # if none are passed: assume default filename for annotations,
+        # under default location
+        if not input_annotation_files:
+            for dataset in dataset_dirs:
+                annotation_files.append(
+                    str(
+                        Path(dataset)
+                        / "annotations"
+                        / DEFAULT_ANNOTATIONS_FILENAME
+                    )
+                )
+
+        # if a list of annotation files/filepaths is passed
+        else:
+            for annot, dataset in zip(input_annotation_files, dataset_dirs):
+                # if the annotation is only filename:
+                # assume file is under 'annotation' directory
+                if Path(annot).name == annot:
+                    annotation_files.append(
+                        str(Path(dataset) / "annotations" / annot)
+                    )
+                # otherwise assume the full path to the annotations file is passed
+                else:
+                    annotation_files.append(annot)
+
+        return annotation_files
+
+    def train_model(self):
+        # Create data module
+        data_module = CrabsDataModule(
+            self.images_dirs,  # list of paths
+            self.annotation_files,  # list of paths
+            self.config,
+            self.seed_n,
         )
 
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         run_name = f"run_{timestamp}"
 
+        # Initialise MLflow logger
         mlf_logger = pl.pytorch.loggers.MLFlowLogger(
             run_name=run_name,
-            experiment_name=args.experiment_name,
+            experiment_name=self.experiment_name,
             tracking_uri="file:./ml-runs",
         )
 
         mlf_logger.log_hyperparams(self.config)
+        mlf_logger.log_hyperparams({"split_seed": self.seed_n})
 
         lightning_model = FasterRCNN(self.config)
 
@@ -72,7 +120,10 @@ class DectectorTrain:
             logger=mlf_logger,
         )
 
+        # Run training
         trainer.fit(lightning_model, data_module)
+
+        # Save model if required
         if self.config["save"]:
             save_model(lightning_model)
 
@@ -94,25 +145,25 @@ def main(args) -> None:
     trainer.train_model()
 
 
-if __name__ == "__main__":
+def train_parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--config_file",
         type=str,
-        default="crabs/detection_tracking/config/faster_rcnn.yaml",
+        default=str(Path(__file__).parent / "config" / "faster_rcnn.yaml"),
         help="location of YAML config to control training",
     )
     parser.add_argument(
-        "--main_dir",
+        "--dataset_dirs",
         nargs="+",
         required=True,
-        help="list of locations of images and coco annotations",
+        help="list of dataset directories",
     )
     parser.add_argument(
-        "--annotation_file",
+        "--annotation_files",
         nargs="+",
-        required=True,
-        help="list of filenames for coco annotations",
+        default=[],
+        help="list of paths to annotation files. The full path or the filename can be provided. If only filename is provided, it is assumed to be under dataset/annotations.",
     )
     parser.add_argument(
         "--accelerator",
@@ -130,8 +181,13 @@ if __name__ == "__main__":
         "--seed_n",
         type=int,
         default=42,
-        help="seed for random state",
+        help="seed for dataset splits",
     )
-    args = parser.parse_args()
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
     torch.set_float32_matmul_precision("medium")
-    main(args)
+
+    train_args = train_parse_args(sys.argv[1:])
+    main(train_args)
