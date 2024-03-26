a03e5c99f077d94df4c8c242a2830a020d02b7f0
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-26 16:53:15 +0000

    update bash script for array job for training

--- bash_scripts/run_training_array.sh
+++ bash_scripts/run_training_array.sh
@@ -2,19 +2,29 @@
 
 #SBATCH -p gpu # partition
 #SBATCH -N 1   # number of nodes
-#SBATCH --mem 8G # memory pool for all cores
-#SBATCH -n 2 # number of cores
+#SBATCH --ntasks-per-node 2 # number of tasks per node
+#SBATCH --mem 64G # memory pool for all cores
+#SBATCH --gres=gpu:1  # any GPU
 #SBATCH -t 3-00:00 # time (D-HH:MM)
-#SBATCH -o slurm_array.%N.%A-%a.out
-#SBATCH -e slurm_array.%N.%A-%a.err
+
+#SBATCH -o slurm_array.%A-%a.%N.out
+#SBATCH -e slurm_array.%A-%a.%N.err
+#SBATCH --array=0-2%3
+
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s.minanon@ucl.ac.uk
-#SBATCH --array=0-2%3
 
 
 # NOTE on SBATCH command for array jobs
 # with "SBATCH --array=0-n%m" ---> runs n separate jobs, but not more than m at a time.
 # the number of array jobs should match the number of input files
+
+# ---------------------
+# Source bashrc
+# ----------------------
+# Otherwise `which python` points to the miniconda module's Python
+source ~/.bashrc
+
 
 # -----------------------------
 # Error settings for bash
@@ -24,21 +34,20 @@ set -e  # do not continue after errors
 set -u  # throw error if variable is unset
 set -o pipefail  # make the pipe fail if any part of it fails
 
-
-
 # ---------------------
 # Define variables
 # ----------------------
 
-# Input files
-CRABS_REPO_LOCATION=/ceph/scratch/sminano/crabs-exploration
+# dataset and train config
 DATASET_DIR=/ceph/zoo/users/sminano/crabs_bboxes_labels/Sep2023_labelled
-TRAIN_CONFIG_FILE=/ceph/scratch/sminano/faster_rcnn.yaml
+TRAIN_CONFIG_FILE=/ceph/scratch/sminano/crabs-exploration/cluster_train_config.yaml
 
+# seeds for each dataset split
 LIST_SEEDS=($(echo {42..44}))
-SEED_SPLIT=${LIST_SEEDS[${SLURM_ARRAY_TASK_ID}]}
+SPLIT_SEED=${LIST_SEEDS[${SLURM_ARRAY_TASK_ID}]}
 
-ENV_NAME=crabs-dev-$SEED_SPLIT
+# version of the codebase
+GIT_BRANCH=smg/train-entry-point
 
 # --------------------
 # Check inputs
@@ -55,19 +64,32 @@ fi
 # -----------------------------
 module load miniconda
 
-conda create -y -n $ENV_NAME python=3.10
-source activate $ENV_NAME
+# create a unique environment for this job
+ENV_NAME=crabs-dev-$SPLIT_SEED-$SLURM_JOB_ID
+conda create -n $ENV_NAME -y python=3.10
+conda activate $ENV_NAME
 
-cd $CRABS_REPO_LOCATION
-python -m pip install .[dev]  # can omit -e if not debugging
+# check pip and python
+which python
+which pip
+
+# install crabs package in virtual env
+# $HOME/.conda/envs/$ENV_NAME
+python -m pip install git+https://github.com/SainsburyWellcomeCentre/crabs-exploration.git@$GIT_BRANCH
 
 
 # -------------------
 # Run training script
 # -------------------
-python "$CRABS_REPO_LOCATION"/crabs/detection_tracking/train_model.py  \
+train-detector  \
  --dataset_dirs $DATASET_DIR \
  --config_file $TRAIN_CONFIG_FILE \
  --accelerator gpu \
  --experiment_name "Sept2023_base_data_augm" \
- --seed_n $SEED_SPLIT \
+ --seed_n $SPLIT_SEED \
+
+# -----------------------------
+# Delete virtual environment
+# -----------------------------
+conda deactivate
+conda remove -n $ENV_NAME --all
