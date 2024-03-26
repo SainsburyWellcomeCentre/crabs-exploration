2fe64bd236953b8365d273abd28b6ff6962c78f3
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-26 16:28:23 +0000

    replace number of cores for ntasks-per-node for lightning

--- bash_scripts/run_training.sh
+++ bash_scripts/run_training.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p gpu # partition
 #SBATCH -N 1   # number of nodes
-#SBATCH -n 2 # number of cores
+#SBATCH --ntasks-per-node 2 # number of tasks per node
 #SBATCH --mem 64G # memory pool for all cores
 #SBATCH --gres=gpu:1  # any GPU
 #SBATCH -t 3-00:00 # time (D-HH:MM)
@@ -35,16 +35,15 @@ set -o pipefail  # make the pipe fail if any part of it fails
 # Define variables
 # ----------------------
 
-# script location
-CRABS_REPO_LOCATION=/ceph/scratch/sminano/crabs-exploration
+# dataset and train config
 DATASET_DIR=/ceph/zoo/users/sminano/crabs_bboxes_labels/Sep2023_labelled
-TRAIN_CONFIG_FILE=$CRABS_REPO_LOCATION/cluster_train_config.yaml
+TRAIN_CONFIG_FILE=/ceph/scratch/sminano/crabs-exploration/cluster_train_config.yaml
 
 # seed for the dataset split
 SPLIT_SEED=42
 
-# create a unique environment for this job
-ENV_NAME=crabs-dev-$SPLIT_SEED-$SLURM_JOB_ID
+# version of the codebase
+GIT_BRANCH=smg/train-entry-point
 
 # -----------------------------
 # Create virtual environment
@@ -52,19 +51,24 @@ ENV_NAME=crabs-dev-$SPLIT_SEED-$SLURM_JOB_ID
 
 module load miniconda
 
+# create a unique environment for this job
+ENV_NAME=crabs-dev-$SPLIT_SEED-$SLURM_JOB_ID
 conda create -n $ENV_NAME -y python=3.10
 conda activate $ENV_NAME
 
+# check pip and python
 which python
 which pip
 
-cd $CRABS_REPO_LOCATION
-python -m pip install .
+# install crabs package in virtual env
+# $HOME/.conda/envs/$ENV_NAME
+# cd $CRABS_REPO_LOCATION
+python -m pip install git+https://github.com/SainsburyWellcomeCentre/crabs-exploration.git@$GIT_BRANCH
 
 # -------------------
 # Run training script
 # -------------------
-python "$CRABS_REPO_LOCATION"/crabs/detection_tracking/train_model.py  \
+train-detector  \
  --dataset_dirs $DATASET_DIR \
  --config_file $TRAIN_CONFIG_FILE \
  --accelerator gpu \
