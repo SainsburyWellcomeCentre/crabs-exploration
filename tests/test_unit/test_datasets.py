ec2fde066bb16f90531593a7a2fc96c81208bb25
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-26 12:08:04 +0000

    prepare for multiple datasets

--- tests/test_unit/test_datasets.py
+++ tests/test_unit/test_datasets.py
@@ -8,18 +8,24 @@ from crabs.detection_tracking.datasets import CrabsCocoDetection
 DATASET_1 = "/home/data/dataset1"
 DATASET_2 = "/home/data/dataset2"
 
-ANNOTATION_FILE = (
+ANNOTATION_FILE_1 = (
     Path(__file__).parents[1] / "data" / "sample_annotations_1.json"
+)
+ANNOTATION_FILE_2 = (
+    Path(__file__).parents[1] / "data" / "sample_annotations_2.json"
 )
 
 
+@pytest.mark.parametrize(
+    "list_datasets, list_annotations",
+    [
+        ([DATASET_1], [ANNOTATION_FILE_1]),
+        ([DATASET_1, DATASET_2], [ANNOTATION_FILE_1, ANNOTATION_FILE_2]),
+    ],
+)
 @pytest.mark.parametrize("n_files_to_exclude", [0, 1, 20])
-def test_exclude_files(n_files_to_exclude):
-    # Define list of input data
-    list_datasets = [DATASET_1]
-    list_annotations = [ANNOTATION_FILE]
-
-    # Create a dataset with all the images in the annotation file
+def test_exclude_files(list_datasets, list_annotations, n_files_to_exclude):
+    # Create a dataset with all the input data
     dataset = CrabsCocoDetection(
         list_datasets,
         list_annotations,
@@ -27,15 +33,16 @@ def test_exclude_files(n_files_to_exclude):
     # Check list of excluded files in dataset is None
     assert dataset.list_exclude_files is None
 
-    # Select a subset of images in the dataset as images to exclude.
-    # If multiple dataset: ensure we sample images from each dataset.
+    # Select a subset of images in the full dataset as images to exclude.
+    # If multiple dataset: sample images from each dataset if possible.
     n_datasets = len(list_datasets)
     q, r = divmod(n_files_to_exclude, n_datasets)
     # If n_files_to_exclude > n_datasets: add remained to last dataset
     if q >= 1:
         n_exclude_per_dataset = [q] * n_datasets
         n_exclude_per_dataset[-1] += r
-    # If n_files_to_exclude < n_datasets: sample from n_files_to_exclude datasets
+    # If n_files_to_exclude < n_datasets:
+    # sample one random image from `n_files_to_exclude` datasets
     else:
         n_exclude_per_dataset = [1] * r + [0] * (n_datasets - r)
         random.shuffle(n_exclude_per_dataset)
