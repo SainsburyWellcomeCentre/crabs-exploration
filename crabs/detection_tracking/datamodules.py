d608c2991fb5ae9ce75022424267ebf80df0ae7f
sfmig <33267254+sfmig@users.noreply.github.com>
2024-03-26 08:08:03 +0000

    Dataloader: datamodule and datasets refactoring (1/3) (#123)
    * readme for datamodule current status
    
    * update config with new keys
    
    * delete previous dataset and datamodule
    
    * new datamodules and datasets modules
    
    * changes to train script for datamodule structure
    
    * changes to evaluate script for datamodule structure
    
    * add docstrings and types
    
    * add types and docstrings
    
    * add types and docstrings
    
    * exclude files from config!
    
    * create a filtered annotation file only if images are actually removed
    
    * allow for exclude_video_file_list not in config
    
    * add some clarifications to accelerator argument
    
    * add conditions for mps
    
    * rename slc to select
    
    * move plot sample convenience function to visualization module
    
    * fix argparser help for evaluate
    
    * rename lightning import
    
    * Apply suggestions from code review
    
    refactor data augmentation transforms
    
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>
    Signed-off-by: sfmig <33267254+sfmig@users.noreply.github.com>
    
    * remove list comprehension
    
    * make modified annotation file a temporary file
    
    * a cleaner syntax to exclude files using tmp file
    
    ---------
    
    Signed-off-by: sfmig <33267254+sfmig@users.noreply.github.com>
    Co-authored-by: Sam Cunliffe <samcunliffe@users.noreply.github.com>

---
+++ crabs/detection_tracking/datamodules.py
@@ -0,0 +1,215 @@
+from typing import Optional
+
+import torch
+import torchvision
+import torchvision.transforms.v2 as transforms
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, random_split
+
+from crabs.detection_tracking.datasets import CrabsCocoDetection
+
+
+class CrabsDataModule(LightningDataModule):
+    """A Lightning DataModule class for the crabs data.
+
+    It encapsulate all the steps needed to process the data:
+    - computing dataset splits
+    - defining the data augmentation transforms
+    - defining the dataloaders
+    """
+
+    def __init__(
+        self,
+        list_img_dirs: list[str],
+        list_annotation_files: list[str],
+        config: dict,
+        split_seed: Optional[int] = None,
+    ):
+        super().__init__()
+        self.list_img_dirs = list_img_dirs
+        self.list_annotation_files = list_annotation_files
+        self.split_seed = split_seed
+        self.config = config
+
+    def _get_train_transform(self) -> torchvision.transforms:
+        """Define data augmentation transforms for the train set.
+
+        Using transforms.v2, for more details see:
+        https://pytorch.org/vision/stable/transforms.html#v1-or-v2-which-one-should-i-use
+        https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#transforms
+
+        """
+        jitter = transforms.ColorJitter(
+            brightness=self.config["transform_brightness"],
+            hue=self.config["transform_hue"],
+        )
+        gauss = transforms.GaussianBlur(
+            kernel_size=self.config["gaussian_blur_params"]["kernel_size"],
+            sigma=self.config["gaussian_blur_params"]["sigma"],
+        )
+        todtype = transforms.ToDtype(torch.float32, scale=True)
+        train_transforms = [transforms.ToImage(), jitter, gauss, todtype]
+        return transforms.Compose(train_transforms)
+
+    def _get_test_val_transform(self) -> torchvision.transforms:
+        """Define data augmentation transforms for the test set.
+
+        Using transforms.v2, for more details see:
+        https://pytorch.org/vision/stable/transforms.html#v1-or-v2-which-one-should-i-use
+        https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#transforms
+
+        """
+        test_transforms = [
+            transforms.ToImage(),
+            transforms.ToDtype(torch.float32, scale=True),
+        ]
+        return transforms.Compose(test_transforms)
+
+    def _collate_fn(self, batch: tuple) -> tuple:
+        """Collate function used for dataloaders.
+
+        A custom function is needed for detection
+        because the number of bounding boxes varies
+        between images of the same batch.
+        See https://pytorch.org/vision/main/auto_examples/transforms/plot_transforms_e2e.html#data-loading-and-training-loop
+
+        Parameters
+        ----------
+        batch : tuple
+            a tuple of 2 tuples, the first one holding all images in the batch,
+            and the second one holding the corresponding annotations.
+
+        Returns
+        -------
+        tuple
+            a tuple of length = batch size, made up of (image, annotations)
+            tuples.
+        """
+        return tuple(zip(*batch))
+
+    def _compute_splits(
+        self,
+    ) -> tuple[CrabsCocoDetection, CrabsCocoDetection, CrabsCocoDetection]:
+        """Compute train/test/validation splits.
+
+        The split is reproducible if the same seed is passed.
+
+        The fraction of samples to use in the train set is defined by
+        the `train_fraction` in the config file.
+
+        The remaining samples are split between test and validation.
+        The fraction of samples to use for validation, over the total
+        of samples to use for train+validation is defined by the
+        `val_over_test_fraction` parameter in the config file.
+
+        Returns
+        -------
+        tuple
+            A tuple with the train, test and validation datasets
+        """
+
+        # Optionally fix the generator for a reproducible split of data
+        generator = None
+        if self.split_seed:
+            generator = torch.Generator().manual_seed(self.split_seed)
+
+        # Create dataset (combining all datasets passed)
+        full_dataset = CrabsCocoDetection(
+            self.list_img_dirs,
+            self.list_annotation_files,
+            transforms=self.train_transform,
+            list_exclude_files=self.config.get(
+                "exclude_video_file_list"
+            ),  # get value only if key exists
+        )
+
+        # Split data into train and test-val sets
+        # TODO: split based on video
+        train_dataset, test_val_dataset = random_split(
+            full_dataset,
+            [self.config["train_fraction"], 1 - self.config["train_fraction"]],
+            generator=generator,
+        )
+
+        # Split test/val sets from the remainder
+        test_dataset, val_dataset = random_split(
+            test_val_dataset,
+            [
+                1 - self.config["val_over_test_fraction"],
+                self.config["val_over_test_fraction"],
+            ],
+        )
+
+        return train_dataset, test_dataset, val_dataset
+
+    def prepare_data(self):
+        """
+        To download data, IO, etc. Useful with shared filesystems,
+        only called on 1 GPU/TPU in distributed.
+        """
+        pass
+
+    def setup(self, stage: str):
+        """Setup the data for training or testing.
+
+        - Define transforms for data augmentation
+        - Define train/test/val datasets as required
+
+        Parameters
+        ----------
+        stage : str
+            Identifies whether to apply the setup for training (stage='fit')
+            or testing (stage='test').
+        """
+
+        # Assign transforms
+        self.train_transform = self._get_train_transform()
+        self.test_transform = self._get_test_val_transform()
+        self.val_transform = self._get_test_val_transform()
+
+        # Assign datasets for dataloader depending on stage
+        # omitting "predict" stage for now
+        train_dataset, test_dataset, val_dataset = self._compute_splits()
+        if stage == "fit":
+            self.train_dataset = train_dataset
+            self.val_dataset = val_dataset
+
+        if stage == "test":
+            self.test_dataset = test_dataset
+
+    def train_dataloader(self) -> DataLoader:
+        """Define dataloader for the training set"""
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.config["batch_size_train"],
+            shuffle=True,  # a shuffled sampler will be constructed
+            num_workers=self.config["num_workers"],
+            collate_fn=self._collate_fn,
+            persistent_workers=True
+            if self.config["num_workers"] > 0
+            else False,
+            multiprocessing_context="fork"
+            if self.config["num_workers"] > 0
+            and torch.backends.mps.is_available()
+            else None,  # see https://github.com/pytorch/pytorch/issues/87688
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        """Define dataloader for the validation set"""
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.config["batch_size_val"],
+            shuffle=False,
+            num_workers=self.config["num_workers"],
+            collate_fn=self._collate_fn,
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        """Define dataloader for the test set"""
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.config["batch_size_test"],
+            shuffle=False,
+            num_workers=self.config["num_workers"],
+            collate_fn=self._collate_fn,
+        )
---
+++ crabs/detection_tracking/datasets.py
@@ -0,0 +1,147 @@
+import json
+import os
+import tempfile
+from typing import Callable, Optional
+
+import torch
+from pycocotools.coco import COCO
+from torchvision.datasets import CocoDetection, wrap_dataset_for_transforms_v2
+
+
+class CrabsCocoDetection(torch.utils.data.ConcatDataset):
+    def __init__(
+        self,
+        list_img_dirs: list[str],
+        list_annotation_files: list[str],
+        transforms: Optional[Callable] = None,
+        list_exclude_files: Optional[list[str]] = None,
+    ):
+        """
+        CocoDetection dataset wrapped for transforms_v2.
+
+        Example usage:
+        > dataset = CrabsCocoDetection([IMAGES_PATH], [ANNOTATIONS_PATH])
+        > sample = dataset[0]
+        > img, annotations = sample
+        > print(type(annotations))  # this is a dictionary
+
+        Should produce a dataset equivalent to one obtained with:
+        > dataset = wrap_dataset_for_transforms_v2(CocoDetection([IMAGES_PATH], [ANNOTATIONS_PATH]))
+        """
+
+        # Create list of transformed-COCO datasets
+        list_datasets = []
+        for img_dir, annotation_file in zip(
+            list_img_dirs, list_annotation_files
+        ):
+            if not list_exclude_files:
+                dataset_coco = CocoDetection(
+                    img_dir,
+                    annotation_file,
+                    transforms=transforms,
+                )
+
+            else:
+                # Check if this annotation file has images to exclude
+                coco_obj = COCO(annotation_file)
+                n_imgs_to_exclude = sum(
+                    [
+                        im["file_name"] in list_exclude_files
+                        for _, im in coco_obj.imgs.items()
+                    ]
+                )
+
+                # If it does: create a tmp annotation file without those images
+                if n_imgs_to_exclude > 0:
+                    _, tmp_path = tempfile.mkstemp(suffix=".json", text=True)
+                    try:
+                        # save modified annotations to tmp file
+                        annotation_file_filtered = self.save_filt_annotations(
+                            annotation_file, list_exclude_files, tmp_path
+                        )
+
+                        # create COCO dataset for detection using tmp file
+                        dataset_coco = CocoDetection(
+                            img_dir,
+                            annotation_file_filtered,
+                            transforms=transforms,
+                        )
+
+                    # ensure tmp path is removed after creating dataset,
+                    # even if there is an error in any previous step.
+                    # See https://security.openstack.org/guidelines/dg_using-temporary-files-securely.html
+                    finally:
+                        os.remove(tmp_path)
+
+            # apply wrapper to use "transforms v2"
+            dataset_transformed = wrap_dataset_for_transforms_v2(dataset_coco)
+            list_datasets.append(dataset_transformed)
+
+        # Concatenate datasets
+        full_dataset = torch.utils.data.ConcatDataset(list_datasets)
+
+        # Update class for this instance
+        self.__class__ = full_dataset.__class__
+        self.__dict__ = full_dataset.__dict__
+
+    def save_filt_annotations(
+        self,
+        annotation_file: str,
+        list_files_to_exclude: list[str],
+        out_filename,
+    ) -> str:
+        """Remove selected images from annotation file and save new file.
+
+        A new annotation file is created, excluding the required images,
+        and without the annotations for those images.
+
+        Parameters
+        ----------
+        annotation_file : str
+            path to file with annotations
+        list_files_to_exclude : list[str]
+            list of filenames to exclude from the dataset
+
+        Returns
+        -------
+        str
+            path to new annotation file
+        """
+        # Read annotation file as a dataset dict
+        with open(annotation_file, "r") as f:
+            dataset = json.load(f)
+
+        # Determine images to exclude
+        image_ids_to_exclude = [
+            im["id"]
+            for im in dataset["images"]
+            if im["file_name"] in list_files_to_exclude
+        ]
+        assert len(image_ids_to_exclude) > 0
+
+        # Remove required images from dataset
+        dataset_imgs = dataset["images"].copy()
+        for im in dataset["images"]:
+            if im["id"] in image_ids_to_exclude:
+                dataset_imgs.remove(im)
+        dataset["images"] = dataset_imgs
+
+        # Determine annotations to exclude
+        annotation_ids_to_exclude = [
+            ann["id"]
+            for ann in dataset["annotations"]
+            if ann["image_id"] in image_ids_to_exclude
+        ]
+
+        # Remove required annotations from dataset
+        dataset_annotations = dataset["annotations"].copy()
+        for annot in dataset["annotations"]:
+            if annot["id"] in annotation_ids_to_exclude:
+                dataset_annotations.remove(annot)
+        dataset["annotations"] = dataset_annotations
+
+        # Write new annotations to file
+        with open(out_filename, "w") as f:
+            json.dump(dataset, f)
+
+        return out_filename
