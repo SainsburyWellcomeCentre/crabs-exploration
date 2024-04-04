92e10b49fc147f9cb68bcda8b77546d9eb2f981c
sfmig <33267254+sfmig@users.noreply.github.com>
2024-04-04 14:26:11 +0000

    create environment in compute node tmpdir

--- bash_scripts/run_training_array.sh
+++ bash_scripts/run_training_array.sh
@@ -61,9 +61,13 @@ fi
 # -----------------------------
 module load miniconda
 
-# create a unique environment for this job
+# create a unique environment for this job in the compute node TMPDIR
+# SLURM_TMPDIR
 ENV_NAME=crabs-dev-$SPLIT_SEED-$SLURM_ARRAY_JOB_ID
-conda create -n $ENV_NAME -y python=3.10
+conda create \
+    --prefix $SLURM_TMPDIR/$ENV_NAME \
+    -y \
+    python=3.10
 conda activate $ENV_NAME
 
 # check pip and python
