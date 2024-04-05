c17361233bee5356fa99b8bc993ac2fe59ca5b3b
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2024-04-05 14:37:12 +0100

    changes based on review

--- crabs/detection_tracking/models.py
+++ crabs/detection_tracking/models.py
@@ -54,16 +54,8 @@ class FasterRCNN(LightningModule):
         total_loss = sum(loss for loss in loss_dict.values())
 
         # Accumulate the loss over each step during the epoch
-        if "total_training_loss" not in self.training_step_outputs:
-            self.training_step_outputs[
-                "total_training_loss"
-            ] = total_loss.item()
-            self.training_step_outputs["num_batches"] = 1
-        else:
-            self.training_step_outputs[
-                "total_training_loss"
-            ] += total_loss.item()
-            self.training_step_outputs["num_batches"] += 1
+        self.training_step_outputs["total_training_loss"] += total_loss.item()
+        self.training_step_outputs["num_batches"] += 1
 
         return total_loss
 
@@ -77,7 +69,10 @@ class FasterRCNN(LightningModule):
         )
 
         # Reset the training_step_outputs for the next epoch
-        self.training_step_outputs = {}
+        self.training_step_outputs = {
+            "total_training_loss": 0.0,
+            "num_batches": 0,
+        }
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
