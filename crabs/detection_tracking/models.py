d376b4ceba42cf3147aa26715fdee898c2432c02
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2024-04-09 16:52:18 +0100

    fix precommit

--- crabs/detection_tracking/models.py
+++ crabs/detection_tracking/models.py
@@ -10,8 +10,6 @@ from torchvision.models.detection import (
 
 from crabs.detection_tracking.evaluate import compute_confusion_matrix_elements
 
-from crabs.detection_tracking.evaluate import compute_confusion_matrix_elements
-
 
 class FasterRCNN(LightningModule):
     """
