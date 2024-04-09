41b25eaf6fdadddd4b0310eddc29b2221a7b73c1
sfmig <33267254+sfmig@users.noreply.github.com>
2024-04-09 12:52:32 +0100

    spurious hash in docstring

--- crabs/detection_tracking/detection_utils.py
+++ crabs/detection_tracking/detection_utils.py
@@ -81,23 +81,23 @@ def save_model(model: torch.nn.Module) -> str:
     """
     Save the trained model.
 
-#     Parameters
-#     ----------
-#     model : torch.nn.Module
-#         The PyTorch model to be saved.
+    Parameters
+    ----------
+    model : torch.nn.Module
+        The PyTorch model to be saved.
 
     Returns
     -------
     str
         Name of the saved model.
 
-#     Notes
-#     -----
-#     This function saves the provided PyTorch model to a file with a unique
-#     filename based on the current date and time. The filename format is
-#     'model_<timestamp>.pt'.
+    Notes
+    -----
+    This function saves the provided PyTorch model to a file with a unique
+    filename based on the current date and time. The filename format is
+    'model_<timestamp>.pt'.
 
-#     """
+    """
     current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     directory = "model"
     os.makedirs(directory, exist_ok=True)
