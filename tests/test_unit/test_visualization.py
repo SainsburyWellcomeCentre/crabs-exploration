cf49d9c2b871622376a43bafad52c9cc7b008c08
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2024-04-12 11:21:09 +0100

    changes for visualization

--- tests/test_unit/test_visualization.py
+++ tests/test_unit/test_visualization.py
@@ -17,98 +17,147 @@ def sample_image():
     return np.zeros((100, 100, 3), dtype=np.uint8)
 
 
-def test_draw_bbox(sample_image):
-    top_left = (10, 10)
-    bottom_right = (50, 50)
-    color = (0, 255, 0)
-
+@pytest.mark.parametrize(
+    "top_left, bottom_right, color, label_text",
+    [
+        ((10, 10), (50, 50), (0, 255, 0), None),
+        ((10, 10), (50, 50), (0, 255, 0), "Test Label"),
+    ],
+)
+def test_draw_bbox(sample_image, top_left, bottom_right, color, label_text):
     draw_bbox(sample_image, top_left, bottom_right, color)
 
-    # Check if bounding box is drawn
-    assert np.any(
-        sample_image[
-            top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]
-        ]
-        == color
+    if label_text:
+        assert np.any(
+            sample_image[10:20, 10:100] == color
+        )  # Check if label text is drawn
+    else:
+        assert np.any(
+            sample_image[
+                top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]
+            ]
+            == color
+        )  # Check if bounding box is drawn
+
+
+@pytest.mark.parametrize(
+    "top_left, bottom_right, color",
+    [
+        ((10, 10), (15, 15), (0, 255, 0)),
+    ],
+)
+def test_draw_bbox_crop(sample_image, top_left, bottom_right, color):
+    draw_bbox(sample_image, top_left, bottom_right, color)
+
+    # crop bbox from image
+    # Add 1 to include the last row and column of the bounding box
+    # slicing in Python excludes the upper bound
+    actual_bbox_crop_rgb = sample_image[
+        int(top_left[1]) : int(bottom_right[1]) + 1,
+        int(top_left[0]) : int(bottom_right[0]) + 1,
+    ]
+
+    # build expected result
+    bbox_thickness = 2
+    crop_size = (
+        bottom_right[1] - top_left[1] + 1,
+        bottom_right[0] - top_left[0] + 1,
     )
 
+    expected_bbox_crop_g = np.pad(
+        np.zeros(
+            (
+                (crop_size[0] - (2 * bbox_thickness)),
+                (crop_size[1] - (2 * bbox_thickness)),
+            )
+        ),
+        ((bbox_thickness,), (bbox_thickness,)),
+        constant_values=((255,), (255,)),
+    ).astype(np.uint8)
 
-def test_draw_bbox_with_label(sample_image):
-    top_left = (10, 10)
-    bottom_right = (50, 50)
-    color = (0, 255, 0)
-    label_text = "Test Label"
+    expected_bbox_crop_g = np.round(expected_bbox_crop_g).astype(np.uint8)
 
-    draw_bbox(sample_image, top_left, bottom_right, color, label_text)
+    # append other channels to build rgb
+    expected_bbox_crop_rgb = np.stack(
+        [
+            np.zeros_like(expected_bbox_crop_g),
+            expected_bbox_crop_g,
+            np.zeros_like(expected_bbox_crop_g),
+        ],
+        axis=-1,
+    )
 
-    # Check if label text is drawn
-    assert np.any(sample_image[10:20, 10:100] == color)
+    assert np.all(expected_bbox_crop_rgb == actual_bbox_crop_rgb)
 
 
-def test_draw_detection():
-    # Test with single image, annotation, and detection
+@pytest.mark.parametrize(
+    "annotations, detections",
+    [
+        (
+            [{"boxes": torch.tensor([[10, 10, 20, 20]])}],
+            [
+                {
+                    "scores": torch.tensor([0.9]),
+                    "labels": torch.tensor([1]),
+                    "boxes": torch.tensor([[30, 30, 40, 40]]),
+                }
+            ],
+        ),
+        (
+            [
+                {"boxes": torch.tensor([[10, 10, 20, 20]])},
+                {"boxes": torch.tensor([[50, 50, 60, 60]])},
+            ],
+            [
+                {
+                    "scores": torch.tensor([0.9]),
+                    "labels": torch.tensor([1]),
+                    "boxes": torch.tensor([[30, 30, 40, 40]]),
+                },
+                {
+                    "scores": torch.tensor([0.8]),
+                    "labels": torch.tensor([2]),
+                    "boxes": torch.tensor([[70, 70, 80, 80]]),
+                },
+            ],
+        ),
+        (
+            [
+                {"boxes": torch.tensor([[10, 10, 20, 20]])},
+                {"boxes": torch.tensor([[50, 50, 60, 60]])},
+            ],
+            None,
+        ),
+    ],
+)
+def test_draw_detection_parametrized(annotations, detections):
     imgs = [torch.rand(3, 100, 100)]
-    annotations = [{"boxes": torch.tensor([[10, 10, 20, 20]])}]
-    detections = [
-        {
-            "scores": torch.tensor([0.9]),
-            "labels": torch.tensor([1]),
-            "boxes": torch.tensor([[30, 30, 40, 40]]),
-        }
-    ]
     image_with_boxes = draw_detection(imgs, annotations, detections)
     assert image_with_boxes is not None
 
-    # Test with multiple annotations, and detections
-    imgs = [torch.rand(3, 100, 100)]
-    annotations = [
-        {"boxes": torch.tensor([[10, 10, 20, 20]])},
-        {"boxes": torch.tensor([[50, 50, 60, 60]])},
-    ]
-    detections = [
-        {
-            "scores": torch.tensor([0.9]),
-            "labels": torch.tensor([1]),
-            "boxes": torch.tensor([[30, 30, 40, 40]]),
-        },
-        {
-            "scores": torch.tensor([0.8]),
-            "labels": torch.tensor([2]),
-            "boxes": torch.tensor([[70, 70, 80, 80]]),
-        },
-    ]
-    image_with_boxes = draw_detection(imgs, annotations, detections)
-    assert image_with_boxes is not None
 
-    # Test with different score thresholds
-    image_with_boxes = draw_detection(
-        imgs, annotations, detections, score_threshold=0.5
-    )
-    assert image_with_boxes is not None
-
-    # Test with missing detections
-    image_with_boxes = draw_detection(imgs, annotations, None)
-    assert image_with_boxes is not None
-
-
+@pytest.mark.parametrize(
+    "detections",
+    [
+        (
+            [
+                {
+                    "scores": torch.tensor([0.9]),
+                    "labels": torch.tensor([1]),
+                    "boxes": torch.tensor([[30, 30, 40, 40]]),
+                },
+                {
+                    "scores": torch.tensor([0.8]),
+                    "labels": torch.tensor([2]),
+                    "boxes": torch.tensor([[70, 70, 80, 80]]),
+                },
+            ],
+        ),
+    ],
+)
 @patch("cv2.imwrite")
 @patch("os.makedirs")
-def test_save_images_with_boxes(
-    mock_makedirs,
-    mock_imwrite,
-):
-    detections = [
-        {
-            "scores": torch.tensor([0.9]),
-            "labels": torch.tensor([1]),
-            "boxes": torch.tensor([[30, 30, 40, 40]]),
-        },
-        {
-            "scores": torch.tensor([0.8]),
-            "labels": torch.tensor([2]),
-            "boxes": torch.tensor([[70, 70, 80, 80]]),
-        },
-    ]
+def test_save_images_with_boxes(mock_makedirs, mock_imwrite, detections):
     trained_model = MagicMock()
     test_dataloader = MagicMock()
     trained_model.return_value = detections
