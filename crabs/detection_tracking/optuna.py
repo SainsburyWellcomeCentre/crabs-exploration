64ba44e9d1a3b1086fa3b04518545c0c0242177b
sfmig <33267254+sfmig@users.noreply.github.com>
2024-06-17 17:48:41 +0100

    Add docstring and type

--- crabs/detection_tracking/optuna.py
+++ crabs/detection_tracking/optuna.py
@@ -5,18 +5,39 @@ import optuna
 
 def compute_optimal_hyperparameters(
     objective_fn: Callable, config_optuna: Dict, direction: str = "maximize"
-):
-    # Create an Optuna study
+) -> dict:
+    """Compute hyperparameters that optimize the objective function.
+
+    Use Optuna to optimize the objective function and return the
+    hyperparameters that maximise or minimise the objective function.
+
+    Parameters
+    ----------
+    objective_fn : Callable
+        A function that takes `trial` as input and returns the value to
+        maximise or minimise (a float).
+    config_optuna : Dict
+        A dictionary with the configuration parameters for Optuna.
+    direction : str, optional
+        Specifies whether the optimisation is to maximise or minimise
+        the objective function. By default "maximize".
+
+    Returns
+    -------
+    dict
+        The optimal parameters computed by Optuna.
+    """
+    # Create an study
     study = optuna.create_study(direction=direction)
 
     # Optimize the objective function
     study.optimize(
-        objective_fn,  # a function that takes trial and returns a float
+        objective_fn,  #
         n_trials=config_optuna["n_trials"],
     )
 
-    # Extract optimal results
-    best_trial = study.best_trial
+    # Extract results
+    best_trial = study.best_trial  # Should we log this?
     best_hyperparameters = best_trial.params
 
     return best_hyperparameters
