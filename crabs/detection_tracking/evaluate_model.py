61609b6a1ddd98325e6dfe0d9f491151e85aa43d
sfmig <33267254+sfmig@users.noreply.github.com>
2024-06-26 12:39:56 +0100

    Use config from ckpt if not passed. Use dataset, annot files and seed from ckpt if not passed.

--- crabs/detection_tracking/evaluate_model.py
+++ crabs/detection_tracking/evaluate_model.py
@@ -1,10 +1,12 @@
 import argparse
 import ast
+import logging
 import os
 import sys
 from pathlib import Path
 
 import lightning
+import torch
 import yaml  # type: ignore
 
 from crabs.detection_tracking.datamodules import CrabsDataModule
@@ -31,25 +33,22 @@ class DetectorEvaluation:
     """
 
     def __init__(self, args: argparse.Namespace) -> None:
-        # inputs
+        # CLI inputs
         self.args = args
-        self.config_file = args.config_file
-        self.load_config_yaml()
 
         # trained model
-        self.checkpoint_path = args.checkpoint_path
+        self.trained_model_path = args.trained_model_path
+        self.config_file = args.config_file
+        self.get_config_from_ckpt()
+        # self.load_config_yaml()  # adds self.config from yaml ----> instead get from ckpt!
 
         # dataset: retrieve from ckpt if possible
-        # maybe a different name?
         self.images_dirs = self.get_img_directories_from_ckpt()  # if defined
-        # maybe in detection utils? - I think good here cause nothing else uses it for now
-        self.annotation_files = (
-            self.get_annotation_files_from_ckpt()
-        )  # if defined
-        self.seed_n = self.get_seed_from_ckpt()
+        self.annotation_files = self.get_annotation_files_from_ckpt()
+        self.seed_n = self.get_cli_arg_from_ckpt("seed_n")
 
         # Hardware
-        self.accelerator = args.accelerator  # --------
+        self.accelerator = args.accelerator
 
         # MLflow
         self.experiment_name = args.experiment_name
@@ -59,22 +58,21 @@ class DetectorEvaluation:
         self.fast_dev_run = args.fast_dev_run
         self.limit_test_batches = args.limit_test_batches
 
-    def load_config_yaml(self):
-        with open(self.config_file, "r") as f:
-            self.config = yaml.safe_load(f)
+        logging.info(f"Images directories: {self.images_dirs}")
+        logging.info(f"Annotation files: {self.annotation_files}")
+        logging.info(f"Seed: {self.seed_n}")
 
-    def get_mlflow_client_from_ckpt(self):
-        # we assume an mlflow ckpt
-
+    def get_mlflow_parameters_from_ckpt(self):
+        """Get MLflow client from ckpt path and associated hparams"""
         import mlflow
 
         # roughly assert the format of the path
-        assert Path(self.checkpoint_path).parent.stem == "checkpoints"
+        assert Path(self.trained_model_path).parent.stem == "checkpoints"
 
         # get mlruns path, experiment and run ID associated to this checkpoint
-        self.ckpt_mlruns_path = str(Path(self.checkpoint_path).parents[3])
-        self.ckpt_experimentID = Path(self.checkpoint_path).parents[2].stem
-        self.ckpt_runID = Path(self.checkpoint_path).parents[1].stem
+        self.ckpt_mlruns_path = str(Path(self.trained_model_path).parents[3])
+        self.ckpt_experimentID = Path(self.trained_model_path).parents[2].stem
+        self.ckpt_runID = Path(self.trained_model_path).parents[1].stem
 
         # create an Mlflow client to interface with mlflow runs
         self.mlrun_client = mlflow.tracking.MlflowClient(
@@ -87,62 +85,84 @@ class DetectorEvaluation:
 
         return params
 
-    def get_img_directories_from_ckpt(self) -> list[str]:
-        # if dataset_dirs is empty: retrieve from ckpt path
-        # We assume we always pass a mlflow chckpoint
-        # would this work with a remote?
-        if not self.args.dataset_dirs:
-            # get mlflow client for the ml-runs folder containing the checkpoint
-            params = self.get_mlflow_client_from_ckpt()
+    def get_config_from_ckpt(self):
+        """Get config from checkpoint if not passed as CLI arg"""
 
-            # get dataset_dirs used in training job
-            train_cli_dataset_dirs = ast.literal_eval(
-                params["cli_args/dataset_dirs"]
-            )
+        # If passed: used passed config
+        if self.config_file:
+            with open(self.config_file, "r") as f:
+                config_dict = yaml.safe_load(f)
 
-            # pass that to prep image directories
-            images_dirs = prep_img_directories(train_cli_dataset_dirs)
-
-        # if not empty, call the regular one
+        # If not passed: used config from ckpt
         else:
-            images_dirs = prep_img_directories(self.args.dataset_dirs)
+            params = self.get_mlflow_parameters_from_ckpt()  # string-dict
+
+            # create a 1-level dict
+            config_dict = {}
+            for p in params:
+                if p.startswith("config"):
+                    config_dict[p.replace("config/", "")] = ast.literal_eval(
+                        params[p]
+                    )
+
+            # format as a 2-levels nested dict
+            # forward slashes indicate a nested dict
+            for key in list(config_dict):  # makes a copy of original keys!
+                if "/" in key:
+                    key_parts = key.split("/")
+                    if key_parts[0] not in config_dict:
+                        config_dict[key_parts[0]] = {
+                            key_parts[1]: config_dict.pop(key)
+                        }  # config_dict[key]}  # initialise
+                    else:
+                        config_dict[key_parts[0]].update(
+                            {key_parts[1]: config_dict.pop(key)}
+                        )
+
+            # check there are no more levels
+            assert all(["/" not in key for key in config_dict])
+
+        self.config = config_dict
+
+    def get_cli_arg_from_ckpt(self, cli_arg_str):
+        """Get CLI argument from checkpoint if not defined"""
+        if getattr(self.args, cli_arg_str):
+            cli_arg = getattr(self.args, cli_arg_str)
+        else:
+            params = self.get_mlflow_parameters_from_ckpt()
+
+            cli_arg = ast.literal_eval(params[f"cli_args/{cli_arg_str}"])
+
+        return cli_arg
+
+    def get_img_directories_from_ckpt(self) -> list[str]:
+        """Get image directories from checkpoint if not defined."""
+        # Get dataset directories from ckpt if not defined
+        dataset_dirs = self.get_cli_arg_from_ckpt("dataset_dirs")
+
+        # Extract image directories
+        images_dirs = prep_img_directories(dataset_dirs)
 
         return images_dirs
 
     def get_annotation_files_from_ckpt(self) -> list[str]:
-        # if no annotation files passed:
-        # retrieve from checkpoint
-        # pdb.set_trace()
-        if not self.args.annotation_files:
-            # get mlflow client for the ml-runs folder containing the checkpoint
-            params = self.get_mlflow_client_from_ckpt()
+        """Get annotation files from checkpoint if not defined.
 
-            train_cli_dataset_dirs = ast.literal_eval(
-                params["cli_args/dataset_dirs"]
-            )
-            train_cli_annotation_files = ast.literal_eval(
-                params["cli_args/annotation_files"]
-            )
+        If annotation_files is not pass as CLI arg to evaluate:
+        retrieve annotation_files from ckpt path.
+        """
 
-            annotation_files = prep_annotation_files(
-                train_cli_annotation_files, train_cli_dataset_dirs
-            )
+        # Get path to input annotation files from ckpt if not defined
+        input_annotation_files = self.get_cli_arg_from_ckpt("annotation_files")
 
-        else:
-            annotation_files = prep_annotation_files(
-                self.args.annotation_files, self.args.dataset_dirs
-            )
+        # Get dataset dirs from ckpt if not defined
+        dataset_dirs = self.get_cli_arg_from_ckpt("dataset_dirs")
+
+        # Extract annotation files
+        annotation_files = prep_annotation_files(
+            input_annotation_files, dataset_dirs
+        )
         return annotation_files
-
-    def get_seed_from_ckpt(self) -> int:
-        if not self.args.seed_n:
-            # get mlflow client for the ml-runs folder containing the checkpoint
-            params = self.get_mlflow_client_from_ckpt()
-
-            seed_n = ast.literal_eval(params["cli_args/seed_n"])
-        else:
-            seed_n = self.args.seed_n
-        return seed_n
 
     def setup_trainer(self):
         """
@@ -181,7 +201,9 @@ class DetectorEvaluation:
         )
 
         # Get trained model
-        trained_model = FasterRCNN.load_from_checkpoint(self.checkpoint_path)
+        trained_model = FasterRCNN.load_from_checkpoint(
+            self.trained_model_path, config=self.config
+        )
 
         # Run testing
         trainer = self.setup_trainer()
@@ -224,7 +246,7 @@ def main(args) -> None:
 def evaluate_parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--checkpoint_path",
+        "--trained_model_path",
         type=str,
         required=True,  # --------- can we pass experiment and run-id?
         help="Location of trained model",
@@ -232,10 +254,10 @@ def evaluate_parse_args(args):
     parser.add_argument(
         "--config_file",
         type=str,
-        default=str(Path(__file__).parent / "config" / "faster_rcnn.yaml"),
+        default="",
         help=(
             "Location of YAML config to control evaluation. "
-            "Default: crabs-exploration/crabs/detection_tracking/config/faster_rcnn.yaml"
+            "Default: '' (the config used to train the model is used)"
         ),
     )
     parser.add_argument(
@@ -243,8 +265,8 @@ def evaluate_parse_args(args):
         nargs="+",
         default=[],  # required=True,
         help=(
-            "List of dataset directories. If none provided, the ones used for "
-            "the ckpt training are used."
+            "List of dataset directories. If none provided, the same datasets used for "
+            "the provided model are used."
         ),
     )
     parser.add_argument(
@@ -316,6 +338,8 @@ def evaluate_parse_args(args):
 
 
 def app_wrapper():
+    torch.set_float32_matmul_precision("medium")
+
     eval_args = evaluate_parse_args(sys.argv[1:])
     main(eval_args)
 
