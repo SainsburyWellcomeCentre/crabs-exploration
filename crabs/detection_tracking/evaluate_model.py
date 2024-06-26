ead4f72d2b1bc11a0bee97b501783c01e755d2bc
sfmig <33267254+sfmig@users.noreply.github.com>
2024-06-26 16:24:53 +0100

    Clean up

--- crabs/detection_tracking/evaluate_model.py
+++ crabs/detection_tracking/evaluate_model.py
@@ -23,7 +23,7 @@ from crabs.detection_tracking.visualization import save_images_with_boxes
 
 class DetectorEvaluation:
     """
-    A class for evaluating an object detector using trained model.
+    A class for evaluating an object detector.
 
     Parameters
     ----------
@@ -40,10 +40,9 @@ class DetectorEvaluation:
         self.trained_model_path = args.trained_model_path
         self.config_file = args.config_file
         self.get_config_from_ckpt()
-        # self.load_config_yaml()  # adds self.config from yaml ----> instead get from ckpt!
 
-        # dataset: retrieve from ckpt if possible
-        self.images_dirs = self.get_img_directories_from_ckpt()  # if defined
+        # dataset: retrieve from ckpt if no CLI arguments are passed
+        self.images_dirs = self.get_img_directories_from_ckpt()
         self.annotation_files = self.get_annotation_files_from_ckpt()
         self.seed_n = self.get_cli_arg_from_ckpt("seed_n")
 
@@ -58,12 +57,13 @@ class DetectorEvaluation:
         self.fast_dev_run = args.fast_dev_run
         self.limit_test_batches = args.limit_test_batches
 
+        logging.info("Dataset")
         logging.info(f"Images directories: {self.images_dirs}")
         logging.info(f"Annotation files: {self.annotation_files}")
         logging.info(f"Seed: {self.seed_n}")
 
     def get_mlflow_parameters_from_ckpt(self):
-        """Get MLflow client from ckpt path and associated hparams"""
+        """Get MLflow client from ckpt path and associated params."""
         import mlflow
 
         # roughly assert the format of the path
@@ -79,21 +79,21 @@ class DetectorEvaluation:
             tracking_uri=self.ckpt_mlruns_path,
         )
 
-        # get params of the run
+        # get parameters of the run
         run = self.mlrun_client.get_run(self.ckpt_runID)
         params = run.data.params
 
         return params
 
     def get_config_from_ckpt(self):
-        """Get config from checkpoint if not passed as CLI arg"""
+        """Get config from checkpoint if config is not passed as a CLI argument."""
 
-        # If passed: used passed config
+        # If config in CLI arguments: used passed config
         if self.config_file:
             with open(self.config_file, "r") as f:
                 config_dict = yaml.safe_load(f)
 
-        # If not passed: used config from ckpt
+        # If not: used config from ckpt
         else:
             params = self.get_mlflow_parameters_from_ckpt()  # string-dict
 
@@ -106,14 +106,15 @@ class DetectorEvaluation:
                     )
 
             # format as a 2-levels nested dict
-            # forward slashes indicate a nested dict
-            for key in list(config_dict):  # makes a copy of original keys!
+            # forward slashes in a key indicate a nested dict
+            for key in list(config_dict):  # list makes a copy of original keys
                 if "/" in key:
                     key_parts = key.split("/")
+                    assert len(key_parts) == 2
                     if key_parts[0] not in config_dict:
                         config_dict[key_parts[0]] = {
                             key_parts[1]: config_dict.pop(key)
-                        }  # config_dict[key]}  # initialise
+                        }
                     else:
                         config_dict[key_parts[0]].update(
                             {key_parts[1]: config_dict.pop(key)}
@@ -125,18 +126,18 @@ class DetectorEvaluation:
         self.config = config_dict
 
     def get_cli_arg_from_ckpt(self, cli_arg_str):
-        """Get CLI argument from checkpoint if not defined"""
+        """Get CLI argument from checkpoint if not in self.args."""
         if getattr(self.args, cli_arg_str):
             cli_arg = getattr(self.args, cli_arg_str)
         else:
             params = self.get_mlflow_parameters_from_ckpt()
-
             cli_arg = ast.literal_eval(params[f"cli_args/{cli_arg_str}"])
 
         return cli_arg
 
     def get_img_directories_from_ckpt(self) -> list[str]:
-        """Get image directories from checkpoint if not defined."""
+        """Get image directories from checkpoint if not passed as CLI argument."""
+
         # Get dataset directories from ckpt if not defined
         dataset_dirs = self.get_cli_arg_from_ckpt("dataset_dirs")
 
@@ -146,11 +147,7 @@ class DetectorEvaluation:
         return images_dirs
 
     def get_annotation_files_from_ckpt(self) -> list[str]:
-        """Get annotation files from checkpoint if not defined.
-
-        If annotation_files is not pass as CLI arg to evaluate:
-        retrieve annotation_files from ckpt path.
-        """
+        """Get annotation files from checkpoint if not passed as CLI argument."""
 
         # Get path to input annotation files from ckpt if not defined
         input_annotation_files = self.get_cli_arg_from_ckpt("annotation_files")
@@ -174,7 +171,7 @@ class DetectorEvaluation:
 
         # Setup logger
         mlf_logger = setup_mlflow_logger(
-            experiment_name=self.experiment_name,  # "Sep2023_evaluation",
+            experiment_name=self.experiment_name,
             run_name=self.run_name,
             mlflow_folder=self.mlflow_folder,
             cli_args=self.args,
