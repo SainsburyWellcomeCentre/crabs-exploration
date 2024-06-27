90da5cbfb52b427695bed420ea78d560f515f6c8
sfmig <33267254+sfmig@users.noreply.github.com>
2024-06-27 14:14:04 +0100

    update notebook

--- notebooks/notebook_data_augm.py
+++ notebooks/notebook_data_augm.py
@@ -6,9 +6,9 @@ from crabs.detection_tracking.visualization import plot_sample
 
 # %%%%%%%%%%%%%%%%%%%
 # Input data
-IMG_DIR = "/Users/sofia/arc/project_Zoo_crabs/sep2023-full/frames"
-ANNOT_FILE = "/Users/sofia/arc/project_Zoo_crabs/sep2023-full/annotations/VIA_JSON_combined_coco_gen.json"
-CONFIG = "/Users/sofia/arc/project_Zoo_crabs/restructure/crabs-exploration/crabs/detection_tracking/config/faster_rcnn.yaml"
+IMG_DIR = "/home/sminano/swc/project_crabs/data/sep2023-full/frames"
+ANNOT_FILE = "/home/sminano/swc/project_crabs/data/sep2023-full/annotations/VIA_JSON_combined_coco_gen.json"
+CONFIG = "/home/sminano/swc/project_crabs/crabs-exploration/crabs/detection_tracking/config/faster_rcnn.yaml"
 SPLIT_SEED = 42
 
 # %%%%%%%%%%%%%%%%%%%%
@@ -19,10 +19,10 @@ with open(CONFIG, "r") as f:
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Create datamodule for the input data
 dm = CrabsDataModule(
-    [IMG_DIR],
-    [ANNOT_FILE],
-    config_dict,
-    SPLIT_SEED,
+    list_img_dirs=[IMG_DIR],
+    list_annotation_files=[ANNOT_FILE],
+    config=config_dict,
+    split_seed=SPLIT_SEED,
 )
 # %%%%%%%%%%%%%%%%%%%%%%%%
 # Setup for train / test
@@ -36,7 +36,8 @@ print(dm.train_transform)
 print(dm.val_transform)
 print(dm.test_transform)
 
-# %%
+# %%%%%%%%%%%%%%%%%%%%%%%%%
+# visualize
 train_dataset = dm.train_dataset
 train_sample = train_dataset[0]
 plot_sample([train_sample])
