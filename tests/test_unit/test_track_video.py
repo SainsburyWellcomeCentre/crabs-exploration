6d390742e5dc0765261787840bc7a2e971cdaa6c
sfmig <33267254+sfmig@users.noreply.github.com>
2024-11-07 12:59:50 +0000

    Fix constructor test

--- tests/test_unit/test_track_video.py
+++ tests/test_unit/test_track_video.py
@@ -10,16 +10,17 @@ from crabs.tracker.track_video import Tracking
 
 @pytest.fixture
 def mock_args():
-    temp_dir = tempfile.mkdtemp()
+    tmp_dir = tempfile.mkdtemp()
 
     return Namespace(
         config_file="/path/to/config.yaml",
         video_path="/path/to/video.mp4",
-        trained_model_path="/path/to/model.ckpt",
-        output_dir=temp_dir,
+        trained_model_path="path/to/model.ckpt",
+        output_dir=tmp_dir,
         accelerator="gpu",
         annotations_file=None,
         save_video=None,
+        save_frames=None,
     )
 
 
@@ -28,33 +29,57 @@ def mock_args():
     new_callable=mock_open,
     read_data="max_age: 10\nmin_hits: 3\niou_threshold: 0.1",
 )
-@patch("yaml.safe_load")
 @patch("cv2.VideoCapture")
-@patch("crabs.tracker.track_video.FasterRCNN.load_from_checkpoint")
-@patch("crabs.tracker.track_video.Sort")
-def test_tracking_setup(
-    mock_sort,
-    mock_load_from_checkpoint,
-    mock_videocapture,
+@patch("crabs.tracker.utils.io.get_video_parameters")
+@patch("crabs.tracker.track_video.get_config_from_ckpt")
+@patch("crabs.tracker.track_video.get_mlflow_parameters_from_ckpt")
+# we patch where the function is looked at, see
+# https://docs.python.org/3/library/unittest.mock.html#where-to-patch
+@patch("yaml.safe_load")
+def test_tracking_constructor(
     mock_yaml_load,
+    mock_get_mlflow_parameters_from_ckpt,
+    mock_get_config_from_ckpt,
+    mock_get_video_parameters,
+    mock_videocapture,
     mock_open,
     mock_args,
 ):
+    # mock reading tracking config from file
     mock_yaml_load.return_value = {
         "max_age": 10,
         "min_hits": 3,
         "iou_threshold": 0.1,
     }
 
-    mock_model = MagicMock()
-    mock_load_from_checkpoint.return_value = mock_model
+    # mock getting mlflow parameters from checkpoint
+    mock_get_mlflow_parameters_from_ckpt.return_value = {
+        "run_name": "trained_model_run_name",
+        "cli_args/experiment_name": "trained_model_expt_name",
+    }
 
+    # mock getting trained model's config
+    mock_get_config_from_ckpt.return_value = {}
+
+    # mock getting video parameters
+    mock_get_video_parameters.return_value = {
+        "total_frames": 614,
+        "frame_width": 1920,
+        "frame_height": 1080,
+        "fps": 60,
+    }
+
+    # mock input video as if opened correctly
     mock_video_capture = MagicMock()
     mock_video_capture.isOpened.return_value = True
     mock_videocapture.return_value = mock_video_capture
 
+    # instantiate tracking interface
     tracker = Tracking(mock_args)
 
+    # check output dir is created correctly
+    # TODO: add asserts for other attributes assigned in constructor
     assert tracker.args.output_dir == mock_args.output_dir
 
+    # delete output dir
     Path(mock_args.output_dir).rmdir()
