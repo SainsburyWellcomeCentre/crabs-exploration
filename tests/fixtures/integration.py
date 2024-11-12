04d3f49e01c9857cc234b53aae8ca37cb45038ab
sfmig <33267254+sfmig@users.noreply.github.com>
2024-11-12 17:59:20 +0000

    Remove home mocking commented out fixture

--- tests/fixtures/integration.py
+++ tests/fixtures/integration.py
@@ -9,37 +9,6 @@ import pytest
 GIN_TEST_DATA_REPO = "https://gin.g-node.org/SainsburyWellcomeCentre/crabs-exploration-test-data"
 
 
-# @pytest.fixture(autouse=True)
-# def mock_home_directory(monkeypatch: pytest.MonkeyPatch):
-#     """Monkeypatch pathlib.Path.home().
-
-#     Instead of returning the usual home path, the
-#     monkeypatched version returns the path to
-#     Path.home() / ".mock-home". This
-#     is to avoid local tests interfering with the
-#     potentially existing user data on the same machine.
-
-#     Parameters
-#     ----------
-#     monkeypatch : pytest.MonkeyPatch
-#        a monkeypatch fixture
-
-#     """
-#     # define mock home path
-#     home_path = Path.home()  # actual home path
-#     mock_home_path = home_path / ".mock-home"
-
-#     # create mock home directory if it doesn't exist
-#     if not mock_home_path.exists():
-#         mock_home_path.mkdir()
-
-#     # monkeypatch Path.home() to point to the mock home
-#     def mock_home():
-#         return mock_home_path
-
-#     monkeypatch.setattr(Path, "home", mock_home)
-
-
 @pytest.fixture(scope="session")
 def pooch_registry() -> dict:
     """Pooch registry for the test data.
@@ -53,15 +22,14 @@ def pooch_registry() -> dict:
         URL and hash of the GIN repository with the test data
 
     """
-    # Use pytest fixture? Should it be wiped out after a session?
-    # Initialise a pooch registry for the test data
+    # Initialise pooch registry
     registry = pooch.create(
         Path.home() / ".crabs-exploration-test-data",
         base_url=f"{GIN_TEST_DATA_REPO}/raw/master/test_data",
     )
 
-    # Download the registry file from GIN to the pooch cache
-    # force to download it every time by using a timestamped file name
+    # Download the registry file from GIN
+    # Force to download it fresh every time by using a timestamped filename
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_registry = pooch.retrieve(
         url=f"{GIN_TEST_DATA_REPO}/raw/master/files-registry.txt",
@@ -75,7 +43,7 @@ def pooch_registry() -> dict:
         file_registry,
     )
 
-    # Remove registry file
+    # Delete registry file
     Path(file_registry).unlink()
 
     return registry
