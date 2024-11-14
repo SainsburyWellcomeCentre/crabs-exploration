462e5cf4349230b3f9038ea17c5be1453d8c9239
sfmig <33267254+sfmig@users.noreply.github.com>
2024-11-14 17:57:19 +0000

    Add paths to data and copy config

--- bash_scripts/run_detect_and_track_array.sh
+++ bash_scripts/run_detect_and_track_array.sh
@@ -47,36 +47,20 @@ CKPT_PATH="/ceph/zoo/users/sminano/ml-runs-all/ml-runs/317777717624044570/40b168
 TRACKING_CONFIG_FILE="/ceph/zoo/users/sminano/cluster_tracking_config.yaml"
 
 # Path to the groundtruth annotations file
-GROUNDTRUTH_ANNOTATIONS_FILE="/ceph/zoo/users/sminano/annotations.json"
+GROUNDTRUTH_ANNOTATIONS_FILE="/ceph/zoo/users/sminano/tracking_groundtruth/04.09.2023-04-Right_RE_test_corrected_ST_csv_SM_corrected_20241029_113207.csv"
+
+# List of videos to run inference on: define VIDEOS_DIR and VIDEO_FILENAME
+# NOTE: if any of the paths have spaces, put the path in quotes, but stopping and re-starting at the wildcard.
+# e.g.: "/ceph/zoo/users/sminano/ml-runs-all/ml-runs-scratch/763954951706829194/"*"/checkpoints"
+# e.g.: "checkpoint-epoch="*".ckpt"
+# List of videos to run inference on
+VIDEOS_DIR="/ceph/zoo/users/sminano/escape_clips_sample"
+VIDEO_FILENAME=*.mov
+mapfile -t LIST_VIDEOS < <(find $VIDEOS_DIR -type f -name $VIDEO_FILENAME)
 
 # Select output
 SAVE_FRAMES=true
 SAVE_VIDEO=true
-
-# List of videos to run inference on: define VIDEOS_DIR and VIDEO_FILENAME
-# Example 1: to evaluate all mp4 videos in a directory
-# VIDEOS_DIR=<path-to-videos>
-# CKPT_FILENAME=*.mp4
-
-# Example 2: to evaluate all 'last' checkpoints of an MLflow experiment
-# VIDEOS_DIR=/ceph/zoo/users/sminano/ml-runs-all/ml-runs-scratch/763954951706829194/*/checkpoints
-# VIDEO_FILENAME=last.ckpt
-
-# Example 3: to evaluate all 'checkpoint-epoch=' checkpoints of an MLflow experiment,
-# VIDEOS_DIR=/ceph/zoo/users/sminano/ml-runs-all/ml-runs-scratch/763954951706829194/*/checkpoints
-# VIDEO_FILENAME=checkpoint-epoch=*.ckpt
-
-# NOTE: if any of the paths have spaces, put the path in quotes, but stopping and re-starting at the wildcard.
-# e.g.: "/ceph/zoo/users/sminano/ml-runs-all/ml-runs-scratch/763954951706829194/"*"/checkpoints"
-# e.g.: "checkpoint-epoch="*".ckpt"
-
-# List of videos to run inference on
-VIDEOS_DIR=">path-to-escape-clips-directory"
-VIDEO_FILENAME=*.mp4
-mapfile -t LIST_VIDEOS < <(find $VIDEOS_DIR -type f -name $VIDEO_FILENAME)
-
-# video used in this job
-INPUT_VIDEO=${LIST_VIDEOS[${SLURM_ARRAY_TASK_ID}]}
 
 # version of the codebase
 GIT_BRANCH=main
@@ -134,20 +118,26 @@ echo "-----"
 # -------------------------
 # Run evaluation script
 # -------------------------
+# video used in this job
+INPUT_VIDEO=${LIST_VIDEOS[${SLURM_ARRAY_TASK_ID}]}
+
 echo "Running inference on $INPUT_VIDEO using trained model at $CKPT_PATH"
 
 # append relevant flag to command if the test set is to be used
-if [ "$SAVE_FRAMES" = "true" ]; then
-    SAVE_FRAMES_FLAG="--save_frames"
-else
-    SAVE_FRAMES_FLAG=""
-fi
+# if [ "$SAVE_FRAMES" = "true" ]; then
+#     SAVE_FRAMES_FLAG="--save_frames"
+# else
+#     SAVE_FRAMES_FLAG=""
+# fi
 
-if [ "$SAVE_VIDEO" = "true" ]; then
-    SAVE_VIDEO_FLAG="--save_video"
-else
-    SAVE_VIDEO_FLAG=""
-fi
+# if [ "$SAVE_VIDEO" = "true" ]; then
+#     SAVE_VIDEO_FLAG="--save_video"
+# else
+#     SAVE_VIDEO_FLAG=""
+# fi
+# Set flags based on boolean variables
+SAVE_FRAMES_FLAG=${SAVE_FRAMES:+--save_frames}
+SAVE_VIDEO_FLAG=${SAVE_VIDEO:+--save_video}
 
 # run detect-and-track command
 detect-and-track-video  \
@@ -158,3 +148,12 @@ detect-and-track-video  \
     --accelerator gpu  \
     $SAVE_FRAMES_FLAG  \
     $SAVE_VIDEO_FLAG
+
+
+
+# copy tracking config to output directory
+shopt -s extglob  # Enable extended globbing
+TARGET_DIR=$(echo tracking_output_* | awk '{print $1}')
+
+cp "$TRACKING_CONFIG_FILE" "$TARGET_DIR"/"$TRACKING_CONFIG_FILE"_$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID
+echo "Copied $TRACKING_CONFIG_FILE to $TARGET_DIR"
