ea6adc6e1ee1a4a79d87bbe23974b36a83b0b6c1
sfmig <33267254+sfmig@users.noreply.github.com>
2024-12-09 22:14:57 +0000

    Remove timestamp

--- tests/fixtures/integration.py
+++ tests/fixtures/integration.py
@@ -1,6 +1,5 @@
 """Pytest fixtures for integration tests."""
 
-from datetime import datetime
 from pathlib import Path
 
 import pooch
@@ -28,20 +27,15 @@ def pooch_registry() -> dict:
         base_url=f"{GIN_TEST_DATA_REPO}/raw/master/test_data",
     )
 
-    # Download the registry file from GIN
-    # Force to download it fresh every time by using a timestamped filename
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # Download only the registry file from GIN
+    # if known_hash = None, the file is always downloaded.
     file_registry = pooch.retrieve(
         url=f"{GIN_TEST_DATA_REPO}/raw/master/files-registry.txt",
         known_hash=None,
-        fname=f"files-registry_{timestamp}.txt",
         path=Path.home() / ".crabs-exploration-test-data",
     )
 
     # Load registry file onto pooch registry
     registry.load_registry(file_registry)
-
-    # Delete registry file
-    Path(file_registry).unlink()
 
     return registry
