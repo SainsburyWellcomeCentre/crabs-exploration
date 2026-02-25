26b3bd1a2d2052cb3b43bd6099e798f8cd110ce5
sfmig <33267254+sfmig@users.noreply.github.com>
2026-02-25 18:45:49 +0000

    Save plots as html (WIP)

--- notebooks/crabs_dataset/01_notebook_plots.py
+++ notebooks/crabs_dataset/01_notebook_plots.py
@@ -10,8 +10,10 @@ Useful references:
 # %%
 # %%
 import io
+import re
 from pathlib import Path
 
+import dask
 import datashader as ds
 import datashader.transfer_functions as tf
 import matplotlib.colors as mcolors
@@ -34,7 +36,7 @@ pio.renderers.default = "browser"
 
 # %%
 # %matplotlib widget
-%matplotlib osx
+# %matplotlib osx
 
 # %%%%%%%%%%%%%%%%
 # Input data
@@ -52,8 +54,6 @@ data_vars_order = [
 
 image_w = 4096
 image_h = 2160
-
-min_frames_per_trajectory = 60
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Read dataset as an xarray datatree
@@ -87,18 +87,20 @@ bar_widths = (
 ) + 1
 bar_edges = ds_video.clip_first_frame_0idx.values
 
+frames_to_min = 1 / (59.94 * 60)
+
 # plot horizontal bar plot
 fig, ax = plt.subplots(1, 1, figsize=(8, 5))
 rects = ax.barh(
     y=np.zeros_like(bar_widths),
-    width=bar_widths,
-    left=bar_edges,
+    width=bar_widths * frames_to_min,
+    left=bar_edges * frames_to_min,
     height=bar_height_plot,
     color=list_colors,
 )
 # add vertical lines for escape frames
 ax.vlines(
-    ds_video.clip_escape_first_frame_0idx.values,
+    ds_video.clip_escape_first_frame_0idx.values * frames_to_min,
     ymin=-bar_height_plot / 2,
     ymax=bar_height_plot / 2,
     color=[
@@ -107,23 +109,36 @@ ax.vlines(
     ],
     linestyle="--",
 )
-ax.set_xlim(0, ds_video.clip_last_frame_0idx.values.max())
+
+# Add text labels to each bar
+for i, rect in enumerate(rects):
+    text_x = rect.get_x() + rect.get_width() / 2
+    text_y = rect.get_y() + rect.get_height() / 2
+    ax.text(
+        text_x,
+        text_y,
+        f"({rect.get_width():.1f})",
+        va="center",
+        ha="center",
+        fontsize=10,
+        color="black",
+    )
+
+ax.set_xlim(0, ds_video.clip_last_frame_0idx.values.max() * frames_to_min)
 ax.set_ylim(
     -bar_height_plot / 2,
     bar_height_plot / 2,
 )
-ax.set_xlabel("frames")
+ax.set_xlabel("time (min)")
 ax.yaxis.set_visible(False)
 
-ax.set_aspect(25_000)
+ax.set_aspect(25_000 * frames_to_min)
 ax.set_title(ds_video.video_id)
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%
 # Visualise trajectories for one clip
 # plots each clip of a video in the browser
-
-ds_video = dt["06.09.2023-01-Right"].ds
 
 
 # Helper function
@@ -143,12 +158,15 @@ def _to_df(pos, time_mask, individuals):
     )
 
 
-# Plot one clip per window
-for clip_id in range(ds_video.clip_id.shape[0]):
+# @dask.delayed
+def plot_clip(clip_id, ds_video):
     # Select a clip
     ds_clip = ds_video.isel(clip_id=clip_id)
+    clip_duration_frames = (
+        ds_clip.clip_last_frame_0idx - ds_clip.clip_first_frame_0idx
+    ).values.item() + 1
 
-    # Define colormap (before filtering for easier comparison)
+    # Define colormap
     individuals_all = ds_clip.individuals.values.astype(str)
     tab20 = plt.cm.tab20.colors  # 20 RGBA tuples
     tab20_hex = [mcolors.to_hex(c) for c in tab20]
@@ -156,17 +174,6 @@ for clip_id in range(ds_video.clip_id.shape[0]):
         str(ind): tab20_hex[i % len(tab20_hex)]
         for i, ind in enumerate(individuals_all)
     }
-
-    # # Filter out short trajectories
-    # n_samples_per_individual = (
-    #     ds_clip.position.notnull().all(dim="space").sum(dim="time").compute()
-    # )
-    # ds_clip = ds_clip.sel(
-    #     individuals=n_samples_per_individual >= min_frames_per_trajectory * 100
-    #     # the mask needs to computed concretely to determine which
-    #     # individuals to keep. If we don't compute n_samples_per_individual
-    #     # explicitly, it will trigger loading the full ds_clip.position into RAM
-    # )
 
     # Get data
     position = ds_clip.position.values  # (time, space, individuals)
@@ -241,66 +248,12 @@ for clip_id in range(ds_video.clip_id.shape[0]):
                 )
             )
 
-    # # outbound
-    # # One trace per individual
-    # for ind_label in np.unique(df_out["ind"]):
-    #     mask = df_out["ind"] == ind_label
-    #     fig.add_trace(
-    #         go.Scattergl(
-    #             x=df_out.loc[mask, "x"],
-    #             y=df_out.loc[mask, "y"],
-    #             mode="markers",
-    #             marker=dict(size=4, color=color_map[ind_label]),
-    #             name=ind_label,
-    #             legendgroup=ind_label,
-    #             showlegend=True,
-    #         )
-    #     )
-
-    # # add marker for first position
-    # for ind_label in df_start_per_individual.index:
-    #     fig.add_trace(
-    #         go.Scattergl(
-    #             x=[df_start_per_individual.loc[ind_label, "x"]],
-    #             y=[df_start_per_individual.loc[ind_label, "y"]],
-    #             mode="markers",
-    #             marker=dict(
-    #                 size=12,
-    #                 symbol="star",
-    #                 color=color_map[ind_label],
-    #                 line=dict(color="black", width=0.5),
-    #             ),
-    #             name=ind_label,
-    #             legendgroup=ind_label,
-    #             showlegend=False,  # avoid duplicate legend entries
-    #         )
-    #     )
-
-    # # inbound
-    # # One trace per individual
-    # for ind_label in np.unique(df_in["ind"]):
-    #     mask = df_in["ind"] == ind_label
-    #     fig.add_trace(
-    #         go.Scattergl(
-    #             x=df_in.loc[mask, "x"],
-    #             y=df_in.loc[mask, "y"],
-    #             mode="markers",
-    #             marker=dict(
-    #                 size=4,
-    #                 color=color_map[ind_label],
-    #                 line=dict(color="red", width=0.75),  # red edge = inbound
-    #             ),
-    #             name=ind_label,
-    #             legendgroup=ind_label,
-    #             showlegend=False,  # avoid duplicate legend entries
-    #         )
-    #     )
-
     fig.update_layout(
         title=(
             f"{ds_video.video_id} - "
             f"{ds_clip.clip_id.values.item()} - "
-            f"{ds_clip.clip_escape_type.values.item()}"
+            f"{ds_clip.clip_escape_type.values.item()} "
+            f"({clip_duration_frames * frames_to_min:.1f} min)"
         ),
         xaxis_title="x (pixels)",
         yaxis_title="y (pixels)",
@@ -330,14 +283,69 @@ for clip_id in range(ds_video.clip_id.shape[0]):
         ),
     )
 
+    return fig
+
+
+# %%
+# Plot one clip per browser tab
+for clip_id in [6]:  # range(ds_video.clip_id.shape[0]):
+    fig = plot_clip(clip_id, dt["06.09.2023-01-Right"].ds)
     fig.show()
+
+    # self-contained HTML (available offline)
+    title = fig.layout.title.text or f"clip_{clip_id}"
+    safe_title = re.sub(r'[\\/*?:"<>|]', "_", title)
+    print(safe_title)
+    fig.write_html(
+        f"/Users/sofia/arc/project_Zoo_crabs/crabs-exploration/{safe_title}.html",
+        include_plotlyjs=True,
+    )
+
+# %%
+# # Save each plot as an svg
+# import re
+# from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+
+# def save_clip_svg(clip_id):
+#     try:
+#         dt = xr.open_datatree(
+#             crabs_zarr_dataset,
+#             engine="zarr",
+#             chunks={},
+#         )
+#         ds_video = dt["06.09.2023-01-Right"].ds
+
+#         fig = plot_clip(clip_id, ds_video)
+#         title = fig.layout.title.text or f"clip_{clip_id}"
+#         safe_title = re.sub(r'[\\/*?:"<>|]', "_", title)
+#         print(safe_title)
+#         # Self-contained HTML (includes Plotly.js inline, ~3MB)
+#         # fig.write_html("figure.html", include_plotlyjs=True)
+
+#         # Or use a CDN link instead (much smaller file)
+#         # requires an internet connection to render.
+#         fig.write_html(
+#             f"/Users/sofia/arc/project_Zoo_crabs/crabs-exploration/{safe_title}.html",
+#             include_plotlyjs="cdn",
+#         )
+#     except Exception as e:
+#         print(f"Error saving clip {clip_id}: {e}")
+
+
+# with ThreadPoolExecutor(max_workers=8) as executor:
+#     executor.map(save_clip_svg, range(ds_video.clip_id.shape[0]))
+
+# # %%
+# # Plot in parallel?
+
+# # tasks = [plot_clip(i, ds_video, image_w, image_h) for i in range(ds_video.clip_id.shape[0])]
+# # dask.compute(*tasks, scheduler="threads")
 
 
 # %%%%%%%%%
 # Plot occupancy heatmap for all clips in same video
-# TODO: add colorbar and count!
-
-ds_video = dt["07.09.2023-04-Right"].ds
+ds_video = dt["06.09.2023-01-Right"].ds
 
 # prepare data
 # flatten to just time and space coords
@@ -350,14 +358,18 @@ position_flat_y = position_y[mask]
 # free original arrays
 del position_x, position_y
 
+n_bins_x = 100
+n_bins_y = round(n_bins_x * (image_h / image_w))
+
 # plot for all clips in video
 fig, ax = plt.subplots()
-ax.hist2d(
+h, _, _, img = ax.hist2d(
     position_flat_x,
     position_flat_y,
-    bins=[200, 100],
+    bins=[n_bins_x, n_bins_y],
     cmap="viridis",
 )
+fig.colorbar(img, ax=ax, label="count")
 ax.invert_yaxis()
 ax.set_xlabel("x (pixels)")
 ax.set_ylabel("y (pixels)")
@@ -366,15 +378,14 @@ ax.set_title(ds_video.video_id)
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Plot occupancy heatmap for all videos of a single day
-# TODO: add colorbar and count!
 
-leaves_pattern = "07.09.2023*"
+leaves_pattern = "06.09.2023*"
 dt_subset_videos = dt.match(leaves_pattern)
 
 # We build histogram incrementally to avoid RAM peaks
 # initialise final histogram data
-n_bins_x = 200
-n_bins_y = 100
+n_bins_x = 100
+n_bins_y = round(n_bins_x * (image_h / image_w))
 bins = [
     np.linspace(0, image_w, n_bins_x + 1),
     np.linspace(0, image_h, n_bins_y + 1),
@@ -391,19 +402,22 @@ for node in dt_subset_videos.leaves:
     del position  # free immediately
 
     mask = ~np.isnan(x) & ~np.isnan(y)
-    video_hist, _, _ = np.histogram2d(x[mask], y[mask], bins=bins, density=False)
+    video_hist, _, _ = np.histogram2d(
+        x[mask], y[mask], bins=bins, density=False
+    )
     final_hist += video_hist
 
     del x, y, mask
 
 # plot final histogram
 fig, ax = plt.subplots(1, 1)
-ax.pcolormesh(
+mesh = ax.pcolormesh(
     bins[0],
     bins[1],
     final_hist.T,
     cmap="viridis",
 )
+fig.colorbar(mesh, ax=ax, label="count")
 ax.invert_yaxis()
 ax.set_xlabel("x (pixels)")
 ax.set_ylabel("y (pixels)")
@@ -420,10 +434,8 @@ ax.set_title(
 day_month = ["09.08", "10.08", "04.09", "05.09", "06.09", "07.09"]
 
 # image size is 4096 × 2160 pixels
-image_w = 4096
-image_h = 2160
 n_bins_x = 200
-n_bins_y = 100
+n_bins_y = round(n_bins_x * (image_h / image_w))
 bins = [
     np.linspace(0, image_w, n_bins_x + 1),
     np.linspace(0, image_h, n_bins_y + 1),
@@ -480,13 +492,13 @@ for d_i, date in enumerate(day_month):
 # Plot all trajectories from one video
 # (we use datashader to rasterise because ~7M points)
 
-dynspread_threshold = 1.0
+dynspread_threshold = 0.975
 n_frames_in_30min = 30 * 60 * 59.94  # video is 59.94 fps
-for dt_video in dt.match("07.09.2023*").leaves[0:1]:  
+for dt_video in dt.match("06.09.2023*").leaves:
     ds_video = dt_video.ds
 
     # # select clips starting before 30min only
-    # mask_first_30min = (ds_video.clip_first_frame_0idx <= n_frames_in_30min).compute()  
+    # mask_first_30min = (ds_video.clip_first_frame_0idx <= n_frames_in_30min).compute()
     # ds_video = ds_video.where(mask_first_30min, drop=True)
     # del mask_first_30min
 
