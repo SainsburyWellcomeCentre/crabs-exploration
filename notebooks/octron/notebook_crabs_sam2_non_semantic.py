d1bda5e36e832870f4389f7dcb2a485ccbb52608
sfmig <33267254+sfmig@users.noreply.github.com>
2026-03-19 16:15:45 +0000

    First draft

--- notebooks/octron/notebook_crabs_sam2_non_semantic.py
+++ notebooks/octron/notebook_crabs_sam2_non_semantic.py
@@ -23,7 +23,6 @@ Dependencies are auto-installed if the script is executed via uv run
 from pathlib import Path
 
 import numpy as np
-import zarr
 from ethology.io.annotations import load_bboxes
 from octron.sam_octron.helpers.build_sam2_octron import build_sam2_octron
 from octron.sam_octron.helpers.sam2_zarr import (
@@ -31,13 +30,21 @@ from octron.sam_octron.helpers.sam2_zarr import (
     mark_frames_annotated,
 )
 from PIL import Image
+from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Input data
 
 # Predictor
-SAM2_CKPT_PATH = "/Users/sofia/swc/project_octron/OCTRON-GUI/octron/sam_octron/checkpoints/sam2.1_hiera_base_plus.pt"
-SAM2_CONFIG_PATH = "/Users/sofia/swc/project_octron/OCTRON-GUI/octron/sam_octron/configs/sam2.1/sam2.1_hiera_b+.yaml"
+SAM_OCTRON_LOCAL_DIR = Path(
+    "/Users/sofia/swc/project_octron/OCTRON-GUI/octron/sam_octron"
+)
+SAM2_CKPT_PATH = (
+    SAM_OCTRON_LOCAL_DIR / "checkpoints" / "sam2.1_hiera_base_plus.pt"
+)
+SAM2_CONFIG_PATH = (
+    SAM_OCTRON_LOCAL_DIR / "configs" / "sam2.1" / "sam2.1_hiera_b+.yaml"
+)
 
 # September groundtruth data
 DATA_DIR = Path("/Users/sofia/swc/CrabLabels/sep2023-full")
@@ -49,7 +56,9 @@ ANNOTATIONS_FILE = DATA_DIR / "annotations" / "VIA_JSON_combined_coco_gen.json"
 LABEL_NAME = "crab"
 
 # Output dir
-OUTPUT_DIR = Path("./output")  # root output folder
+OUTPUT_DIR = Path(
+    "/Users/sofia/arc/project_Zoo_crabs/crabs-exploration/notebooks"
+)  # root output folder
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%
 # Helpers
@@ -77,13 +86,6 @@ class ImageArrayLazy:
         # B, H, W, C
 
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Create output masks dir
-# data_hash = DATA_DIR.name
-# annotation_dir = CRAB_MASKS_DIR / data_hash
-# annotation_dir.mkdir(parents=True, exist_ok=True)
-
-
 # %%%%%%%%%%%%%%%%%%%%%%
 # Read groundtruth as ethology annotation dataset
 ds_bboxes = load_bboxes.from_files(
@@ -106,44 +108,20 @@ ds_bboxes.attrs["image_array"] = image_array
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Load predictor
-predictor, device = build_sam2_octron(
+# use instead:?
+# image_predictor = SAM2ImagePredictor.from_pretrained(
+#   "facebook/sam2.1-hiera-base-plus"
+# )
+model, device = build_sam2_octron(
     config_file_path=SAM2_CONFIG_PATH,
     ckpt_path=SAM2_CKPT_PATH,
 )
 
-# octron flag to track if init_state() has been called
-predictor.is_initialized = False
+image_predictor = SAM2ImagePredictor(model)
 
-
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Initialise zarr cache for predictor
-# When the predictor accesses a frame for the first time
-# it loads + resizes it from disk and writes it into the zarr;
-# on subsequent accesses it reads from the cache instead of re-decoding.
-# This keeps memory usage flat for long videos.
-
-sam_img_size = predictor.image_size  # 1024 for SAM2
-store = zarr.storage.MemoryStore()
-zarr_array = zarr.create_array(
-    store=store,
-    shape=(image_array.shape[0], 3, sam_img_size, sam_img_size),
-    dtype="float16",
-    fill_value=np.nan,
-)
-
-# initialise inference state with data
-# (connects predictor to image data and cache)
-predictor.init_state(video_data=image_array, zarr_store=zarr_array)
-predictor.is_initialized = True
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Define per-frame exemplar bboxes from groundtruth
-
-ds_bboxes = load_bboxes.from_files(
-    ANNOTATIONS_FILE,
-    format="COCO",
-    images_dirs=IMAGES_DIR,
-)
 
 # corner 1 is min x, min y
 # corner 2 is max x, max y -- check!
@@ -183,30 +161,34 @@ mask_zarr = create_image_zarr(
 img_h, img_w = image_array.shape[1], image_array.shape[2]
 
 for frame_idx, bboxes in map_frame_idx_to_boxes.items():
-    # Reset predictor state so object IDs don't accumulate across frames
-    # clears tracking related state so that obj_id starts 0 in next frame etc
-    predictor.reset_state()
+    # Compute image embeddings
+    image_predictor.set_image(image_array[frame_idx])
 
-    # ------
-    # 0 = background
+    # Initialise id_mask:
+    # an integer array (H, W) where each pixel stores which object "owns" it
+    # 0 = background, rest are 1-based object instance IDs
     id_mask = np.zeros((img_h, img_w), dtype=np.int16)
-    for obj_id, box in enumerate(bboxes):
-        # box is [x1, y1, x2, y2] in pixel coords
-        _, _, video_res_masks = predictor.add_new_points_or_box(
-            frame_idx=frame_idx,
-            obj_id=obj_id,
+
+    # Loop thru boxes to predict each mask
+    for box_id, box in enumerate(bboxes):
+        # Predict mask for a single box
+        # XYXY format
+        masks, _scores, _low_res_logits = image_predictor.predict(
             box=box,
-        )
-        # video_res_masks are logits, so we use > 0.0 to get binary masks
-        # (num_objects, 1, H, W) logits — threshold to get binary
-        binary = (video_res_masks[obj_id, 0] > 0.0).cpu().numpy()
-        id_mask[(binary) & (id_mask == 0)] = obj_id + 1  # 1-based IDs
+            multimask_output=False,
+            return_logits=False,
+        )  # masks is (1,H,W) if multimask_output=False
+
+        # assign object ID where mask is True and
+        # id is currently background
+        id_mask[masks[0].astype(bool) & (id_mask == 0)] = box_id + 1
 
     if id_mask.max() == 0:
         print(f"Frame {frame_idx}: no objects segmented")
         continue
-    print(f"Frame {frame_idx}: segmented {int(id_mask.max())} objects")
-    # ---------------------
+    print(
+        f"Frame {frame_idx}: segmented {int(id_mask.max())}/{bboxes.shape[0]} objects"
+    )
 
     # Save id_mask to zarr store
     mask_zarr[frame_idx] = id_mask
@@ -220,6 +202,10 @@ print(
 )
 # %%%%%%%%%%%%%%%%%%%%%%%
 # Review in napari
+
+
+# %%%%%%%%%%%%%%%%
+# Can I add masks to ds_bboxes? aligned with ids?
 
 # %%%%%%%%%%%%%%%%%%%%%%%
 # Generate YOLO detection training data.
