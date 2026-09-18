d09372d41160e72e88054d14880a3deb7df6b6d3
sfmig <33267254+sfmig@users.noreply.github.com>
2026-09-18 15:24:53 +0100

    Revert "Check http?"
    This reverts commit 9a57dd1a5c3b929a1e8dd380658961bebb00b352.

--- tests/fixtures/integration.py
+++ tests/fixtures/integration.py
@@ -5,9 +5,7 @@ from pathlib import Path
 import pooch
 import pytest
 
-GIN_TEST_DATA_REPO = (
-    "http://gin.g-node.org/SainsburyWellcomeCentre/crabs-exploration-test-data"
-)
+GIN_TEST_DATA_REPO = "https://gin.g-node.org/SainsburyWellcomeCentre/crabs-exploration-test-data"
 
 
 @pytest.fixture(scope="session")
