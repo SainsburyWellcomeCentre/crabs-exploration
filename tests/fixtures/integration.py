cab4ceb4b3b2d5e2d9fe352cb6de4fd5e8209388
sfmig <33267254+sfmig@users.noreply.github.com>
2026-09-18 15:26:28 +0100

    Try swc gin

--- tests/fixtures/integration.py
+++ tests/fixtures/integration.py
@@ -5,7 +5,9 @@ from pathlib import Path
 import pooch
 import pytest
 
-GIN_TEST_DATA_REPO = "https://gin.g-node.org/SainsburyWellcomeCentre/crabs-exploration-test-data"
+GIN_TEST_DATA_REPO = (
+    "https://swc.ucl.ac.uk/SainsburyWellcomeCentre/crabs-exploration-test-data"
+)
 
 
 @pytest.fixture(scope="session")
